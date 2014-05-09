17ff40d33f472462a09771e53a826ce84e031c86
pcbaldwin <pbaldwin@clearfoundation.com>
2014-05-08 20:35:58 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'antivirus';
-$app['version'] = '1.5.20';
+$app['version'] = '1.6.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-antivirus.spec
+++ packaging/app-antivirus.spec
@@ -1,7 +1,7 @@
 
 Name: app-antivirus
 Epoch: 1
-Version: 1.5.20
+Version: 1.6.0
 Release: 1%{dist}
 Summary: Gateway Antivirus
 License: GPLv3
