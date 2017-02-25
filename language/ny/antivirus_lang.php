bf93145ce22bb29a7d6c67b963bc6b848022a6f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:11:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The app Polakwika antivayirasi amagwiritsa chapakati antivayirasi injini kuti aone ukonde, FTP, makalata ndi zambiri. Kumatetezera zipangizo olumikiza maukonde anu ndi kuima yaumbanda asanafike mwayi iyambe kugwiritsidwa ntchito yanu.';
+$lang['antivirus_app_name'] = 'pachipata antivayirasi';
+$lang['antivirus_app_tooltip'] = 'owona Encrypted Mankhwalawa amagwiritsidwa ntchito kupulumutsa yaumbanda, kotero chonde samalani za kulola mitundu owona mwa injini antivayirasi.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block encrypted Archive phindu ili udzakhala osazindikilika.';
+$lang['antivirus_block_encrypted_files'] = 'Block Encrypted owona';
+$lang['antivirus_every_two_hours'] = 'Mawola awiri';
+$lang['antivirus_maximum_file_size'] = 'Pazipita Buku Kukula mu Zipi owona';
+$lang['antivirus_maximum_file_size_invalid'] = 'Pazipita file kukula ndi opanda pake.';
+$lang['antivirus_maximum_files'] = 'Pazipita owona mu Zipi owona';
+$lang['antivirus_maximum_files_invalid'] = 'Madera angapo owona ndi opanda pake.';
+$lang['antivirus_maximum_recursion'] = 'Pazipita Recursion mu Zipi owona';
+$lang['antivirus_maximum_recursion_invalid'] = 'Pazipita recursion ndi opanda pake.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking jambulani boma ili udzakhala osazindikilika.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL ulalo mismatch jambulani boma ili udzakhala osazindikilika.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'boma Jambulani maulalo ndi opanda pake.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Njira yofuna anasaina aone boma ili udzakhala osazindikilika.';
+$lang['antivirus_twice_a_day'] = 'Kawiri tsiku';
+$lang['antivirus_update_interval'] = 'pomwe imeneyi';
+$lang['antivirus_updates_per_day_invalid'] = 'Chiwerengero cha zosintha pa tsiku ili udzakhala osazindikilika.';
