cc92e686ea99a5a57720c26fc9d2d4a1de7f2c77
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:16:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Ka whakamahia e te taupānga Gateway pareketo te pūkaha wheori pokapū ki te matawai tukutuku, FTP, mēra me te ake. tiaki te reira i whakaaro hono ki tou whatunga mā te tu pūmanawa i mua i te mea he whai wāhi ki te tae koutou kaiwhakamahi.';
+$lang['antivirus_app_name'] = 'Gateway pareketo';
+$lang['antivirus_app_tooltip'] = 'Kei te maha ngā kōnae whakamuna whakamahia ki te whakaora pūmanawa, na tēnā kia tupato e pā ana ki te tuku i enei momo o kōnae i roto i te pūkaha wheori.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'He muhu Poraka uara pūranga whakamunatia.';
+$lang['antivirus_block_encrypted_files'] = 'Kōnae whakamunatia Poraka';
+$lang['antivirus_every_two_hours'] = 'Nga rua Haora';
+$lang['antivirus_maximum_file_size'] = 'Rahi Kōnae Mōrahi i roto i Kōnae Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'He muhu rahi kōnae mōrahi.';
+$lang['antivirus_maximum_files'] = 'Kōnae mōrahi i roto i Kōnae Zip';
+$lang['antivirus_maximum_files_invalid'] = 'He muhu tau mōrahi o ngā kōnae.';
+$lang['antivirus_maximum_recursion'] = 'Mōrahi Recursion i Kōnae Zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'He muhu recursion mōrahi.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'He muhu SSL āhua scan rawangia.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'He muhu SSL URL ōritengakore kāwanatanga scan.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'He muhu Matawai URL kāwanatanga.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Hītinihanga waitohu karapa kāwanatanga he muhu.';
+$lang['antivirus_twice_a_day'] = 'Rua te Day';
+$lang['antivirus_update_interval'] = 'Whakahōu Wā';
+$lang['antivirus_updates_per_day_invalid'] = 'He muhu maha o whakahōunga ia ra.';
