b151becf831fd1ca6ff72f5ac5571efabde2b9a9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:14:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Úsáideann an app Gateway Antivirus inneall antivirus lárnach a scanadh gréasáin, FTP, post agus níos mó. Cosnaíonn sé feistí ceangailte le do líonra ag stopadh malware sula tá sé deis a bhaint amach do úsáideoirí.';
+$lang['antivirus_app_name'] = 'Geata Antivirus';
+$lang['antivirus_app_tooltip'] = 'comhaid criptithe a úsáidtear go minic malware a sheachadadh, le do thoil a bheith cúramach faoi ligean na cineálacha comhaid tríd an t-inneall antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Is bloc luach cartlainne criptithe neamhbhailí.';
+$lang['antivirus_block_encrypted_files'] = 'Bloc Comhaid Criptithe';
+$lang['antivirus_every_two_hours'] = 'Gach dhá uair an chloig';
+$lang['antivirus_maximum_file_size'] = 'Uasmhéid Comhad i Comhaid Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'Is é méid comhaid uasmhéid neamhbhailí.';
+$lang['antivirus_maximum_files'] = 'Comhaid Uasmhéid in Comhaid Zip';
+$lang['antivirus_maximum_files_invalid'] = 'Is Uasmhéid de chomhaid neamhbhailí.';
+$lang['antivirus_maximum_recursion'] = 'Athchúrsáil Uasmhéid in Comhaid Zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'Is recursion Uasmhéid neamhbhailí.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'Is SSL stáit scanadh cloaking neamhbhailí.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'Is mismatch URL SSL stáit scanadh neamhbhailí.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Is Scan URLanna stáit neamhbhailí.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing sínithe scanadh stáit neamhbhailí.';
+$lang['antivirus_twice_a_day'] = 'Dhá uair Lá';
+$lang['antivirus_update_interval'] = 'Nuashonraigh Eatramh';
+$lang['antivirus_updates_per_day_invalid'] = 'Tá Líon na nuashonruithe sa lá neamhbhailí.';
