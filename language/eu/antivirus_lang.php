35e08c8a40301138de75dfa511c713e485209aa2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:10:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus aplikazioa central antivirus motor bat erabiltzen du web, FTP, mail eta gehiago eskaneatu. Zure sarera konektatuta malware geldiarazteko aukera bat zure erabiltzaile iristeko aurretik by gailu babesten du.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Enkriptatutako fitxategiak askotan erabiltzen dira malware emateko, beraz, mesedez, antivirus motorraren bidez fitxategi mota horietako ahalbidetuz buruz kontuz ibili.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block enkriptatutako artxiboa balio.';
+$lang['antivirus_block_encrypted_files'] = 'Block Enkriptatutako Fitxategiekin';
+$lang['antivirus_every_two_hours'] = 'Bi orduz behin';
+$lang['antivirus_maximum_file_size'] = 'Gehienezko fitxategi Zip Fitxategiak Tamaina';
+$lang['antivirus_maximum_file_size_invalid'] = 'Gehienezko tamaina ez da egokia.';
+$lang['antivirus_maximum_files'] = 'Zip Fitxategiak Gehienezko Files';
+$lang['antivirus_maximum_files_invalid'] = 'fitxategi-kopurua ez da zuzena.';
+$lang['antivirus_maximum_recursion'] = 'Gehienezko Errekurtsio Zip Fitxategiak';
+$lang['antivirus_maximum_recursion_invalid'] = 'Gehienezko errekurtsio baliogabea da.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking eskaneatu egoera baliogabea da.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch eskaneatu egoera baliogabea da.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLak egoera baliogabea da.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'sinadurak eskaneatu egoera Phishing baliogabea da.';
+$lang['antivirus_twice_a_day'] = 'Egunean bi aldiz';
+$lang['antivirus_update_interval'] = 'eguneratze-tartea';
+$lang['antivirus_updates_per_day_invalid'] = 'eguneko eguneraketak zenbakia baliogabea da.';
