9f8f3c1d4042f40e12c3f203cbb87d466865c244
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:19:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The app e heke antiviru sebelisa bohareng antiviru enjene go Scan web, FTP, mangolo le ho feta. E sireletsa disebediswa tse amanang le ho hokahanngoa hao ka emisa malware pele e na le monyetla oa ho finyella basebedisi ba hao.';
+$lang['antivirus_app_name'] = 'heke antiviru';
+$lang['antivirus_app_tooltip'] = 'difaele patilwe ba atisa ho a sebelisa ho lopolla malware, kahoo ka kōpo hlokomela ka bang lumella mefuta eno ya difaele ho pholletsa le enjene antiviru.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block patilwe akhaefe latela boleng ba ha o sebetse.';
+$lang['antivirus_block_encrypted_files'] = 'Block patilwe Files';
+$lang['antivirus_every_two_hours'] = 'Mong le e mong lihora tse peli';
+$lang['antivirus_maximum_file_size'] = 'Palo e kahodimodimo File Size a Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Palo e kahodimodimo saese ya faele ha o sebetse.';
+$lang['antivirus_maximum_files'] = 'Palo e kahodimodimo Files a Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Palo e kahodimodimo palo ea difaele ha o sebetse.';
+$lang['antivirus_maximum_recursion'] = 'Palo e kahodimodimo recursion iphethaphetha a Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Palo e kahodimodimo iphethaphetha ha o sebetse.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan ninemanga.com puso ha o sebetse.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch scan ninemanga.com puso ha o sebetse.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'puso Scan URLs ha o sebetse.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing ditshaeno Scan puso ha o sebetse.';
+$lang['antivirus_twice_a_day'] = 'Ka makhetlo a mabeli letsatsi le';
+$lang['antivirus_update_interval'] = 'Update karohano';
+$lang['antivirus_updates_per_day_invalid'] = 'Palo ea diapdeite ka letsatsi ha o sebetse.';
