011a593b53e76e9381d80f3dc88c223318a11f23
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:17:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Aplikasi Gateway Antivirus menggunakan enjin antivirus pusat untuk mengimbas web, FTP, mel dan banyak lagi. Ia melindungi peranti disambungkan ke rangkaian anda dengan menghentikan malware sebelum ia mempunyai peluang untuk mencapai pengguna anda.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'fail yang disulitkan sering digunakan untuk menyampaikan malware, jadi sila berhati-hati membolehkan jenis fail melalui enjin antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block disulitkan nilai arkib tidak sah.';
+$lang['antivirus_block_encrypted_files'] = 'Block Encrypted Files';
+$lang['antivirus_every_two_hours'] = 'Setiap Dua Jam';
+$lang['antivirus_maximum_file_size'] = 'Saiz Fail Maksimum dalam Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Saiz fail maksimum adalah tidak sah.';
+$lang['antivirus_maximum_files'] = 'Fail-fail maksimum dalam Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Bilangan fail maksimum adalah tidak sah.';
+$lang['antivirus_maximum_recursion'] = 'Rekursi maksimum dalam Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'rekursi maksimum adalah tidak sah.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking negeri imbasan tidak sah.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL tidak sepadan negeri imbasan tidak sah.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'negeri Scan URL tidak sah.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing tandatangan mengimbas negeri adalah tidak sah.';
+$lang['antivirus_twice_a_day'] = 'Dua kali sehari';
+$lang['antivirus_update_interval'] = 'Update Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Bilangan kemas kini setiap hari adalah tidak sah.';
