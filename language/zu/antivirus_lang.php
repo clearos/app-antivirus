10044b8ff8eceae4c3e96dec8bdf0732d2f9a9de
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:22:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Insiza Gateway Antivirus lusebenzisa central unqulo wesi arab injini ukuskena web, FTP, mail nokuningi. Kuvikela amadivayisi kunethiwekhi yakho ngovimba malware ngaphambi it has a chance ukuthi ufinyelele kubasebenzisi bakho.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Amafayela ezibetheliwe avame ukusetshenziswa ukuze akhulule malware, ngakho sicela uqaphele mayelana nokuvumela lezo zinhlobo amafayela ngokusebenzisa injini unqulo wesi arab.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block archive zombhalo alivumelekile.';
+$lang['antivirus_block_encrypted_files'] = 'Block olubethelwe Amafayela';
+$lang['antivirus_every_two_hours'] = 'Njalo ngemva kwamahora amabili';
+$lang['antivirus_maximum_file_size'] = 'Maximum Usayizi Wefayela e Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Usayizi wefayela ophezulu ayivumelekile.';
+$lang['antivirus_maximum_files'] = 'Amafayela Maximum in Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Isibalo esiphezulu amafayela ayivumelekile.';
+$lang['antivirus_maximum_recursion'] = 'Recursion Maximum in Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion Maximum ayivumelekile.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL Ukufihla isimo scan ayivumelekile.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL ayifani isimo scan ayivumelekile.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLs state ayivumelekile.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'IHluzo nesignesha scan state ayivumelekile.';
+$lang['antivirus_twice_a_day'] = 'Kabili Day';
+$lang['antivirus_update_interval'] = 'Update isikhawu';
+$lang['antivirus_updates_per_day_invalid'] = 'Number of updates ngosuku ayivumelekile.';
