1107a631bf640893be638d750bf88a9be8aef208
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:16:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Ny Gateway Antivirus rindrambaiko mampiasa ny fototry ny scan Antivirus maotera aterineto, FTP, namany Sary sy ny maro hafa. Tsy miaro ny fitaovana mifandray amin&#39;ny tambajotra amin&#39;ny alalan&#39;ny malware nijanona teo anoloany izy manana fahafahana hanatratra ny mpampiasa.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Voaaro raki-daza dia matetika ampiasaina mba hamonjy malware, koa masìna ianao, tandremo tsara ny famelana ireo karazana antontan-taratasy ny alalan &#39;ny Antivirus maotera.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block misy mari-pamantarana Archive sarobidy dia tsy manan-kery.';
+$lang['antivirus_block_encrypted_files'] = 'Block voaaro Files';
+$lang['antivirus_every_two_hours'] = 'Isaky ny adiny roa';
+$lang['antivirus_maximum_file_size'] = 'Maximum File Size amin&#39;ny Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maximum file size dia tsy manan-kery.';
+$lang['antivirus_maximum_files'] = 'Maximum Files in Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Maximum isa ny antontan-taratasy dia tsy manan-kery.';
+$lang['antivirus_maximum_recursion'] = 'Maximum Recursion in Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximum recursion dia tsy manan-kery.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking fizahana fanjakana dia tsy manan-kery.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch Notarafina fanjakana dia tsy manan-kery.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLs fanjakana dia tsy manan-kery.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing sonia scan fanjakana dia tsy manan-kery.';
+$lang['antivirus_twice_a_day'] = 'In-droa isan&#39;andro';
+$lang['antivirus_update_interval'] = 'Update elanelam-potoana io';
+$lang['antivirus_updates_per_day_invalid'] = 'Isan&#39;ny vaovao isan&#39;andro dia tsy manan-kery.';
