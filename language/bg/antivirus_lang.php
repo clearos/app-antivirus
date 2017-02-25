4ad85e5c0c7e08d21f0735c0e2edb304f84ed5db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:11:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Приложението за Gateway Antivirus използва централна антивирусен двигател за сканиране на уеб, FTP, поща и др. Той предпазва устройства, свързани към мрежата чрез спиране на злонамерен софтуер, преди да има шанс да достигне до потребителите си.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Криптирани файлове често се използват, за да достави на зловреден софтуер, затова моля бъдете внимателни, за който позволява тези типове файлове чрез антивирусна двигателя.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блок шифрован архив стойност е невалидна.';
+$lang['antivirus_block_encrypted_files'] = 'Блок криптирани файлове';
+$lang['antivirus_every_two_hours'] = 'На всеки два часа';
+$lang['antivirus_maximum_file_size'] = 'Максимален размер на файла в Zip файлове';
+$lang['antivirus_maximum_file_size_invalid'] = 'Максималният размер на файла е невалиден.';
+$lang['antivirus_maximum_files'] = 'Максимални файлове в Zip файлове';
+$lang['antivirus_maximum_files_invalid'] = 'Максимален брой файлове е невалиден.';
+$lang['antivirus_maximum_recursion'] = 'Максимална рекурсия в Zip файлове';
+$lang['antivirus_maximum_recursion_invalid'] = 'Максимална рекурсия е невалиден.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL прикриване състояние сканиране е невалиден.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL разминаване сканиране състояние е невалиден.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL адреси състояние е невалиден.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Фишинг подписи сканират състояние е невалиден.';
+$lang['antivirus_twice_a_day'] = 'Два пъти дневно';
+$lang['antivirus_update_interval'] = 'Актуализация Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Брой актуализации на ден е невалиден.';
