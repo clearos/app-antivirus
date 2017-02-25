4367383710292a71d2be792d73c2a735f9cc2055
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:21:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app nlo a aringbungbun antivirus engine lati ọlọjẹ ayelujara, FTP, mail ati siwaju sii. O aabo awọn ẹrọ ti sopọ si nẹtiwọki rẹ nipa idekun malware ṣaaju ki o to o ni o ni a ni anfani lati de ọdọ rẹ olumulo.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Ti paroko awọn faili ti wa ni igba ti a lo lati fi malware, ki jọwọ wa ni ṣọra nipa gbigba awon orisi ti awọn faili nipasẹ awọn antivirus engine.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Àkọsílẹ ti paroko pamosi iye ti wa ni invalid.';
+$lang['antivirus_block_encrypted_files'] = 'Block ti paroko faili';
+$lang['antivirus_every_two_hours'] = 'Gbogbo meji Wakati';
+$lang['antivirus_maximum_file_size'] = 'O pọju File Iwon ni Zip faili';
+$lang['antivirus_maximum_file_size_invalid'] = 'O pọju faili iwọn ti wa ni invalid.';
+$lang['antivirus_maximum_files'] = 'O pọju faili ni Zip faili';
+$lang['antivirus_maximum_files_invalid'] = 'O pọju nọmba ti awọn faili ti wa ni invalid.';
+$lang['antivirus_maximum_recursion'] = 'O pọju Recursion ni Zip faili';
+$lang['antivirus_maximum_recursion_invalid'] = 'O pọju recursion ni invalid.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking ọlọjẹ ipinle jẹ invalid.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch scan ipinle jẹ invalid.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Wíwo URL ipinle jẹ invalid.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Ararẹ ibuwọlu ọlọjẹ ipinle jẹ invalid.';
+$lang['antivirus_twice_a_day'] = 'Lẹmeji Day';
+$lang['antivirus_update_interval'] = 'Update arin';
+$lang['antivirus_updates_per_day_invalid'] = 'Nọmba ti awọn imudojuiwọn fun ọjọ kan ni invalid.';
