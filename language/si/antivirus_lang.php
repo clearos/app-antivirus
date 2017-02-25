688ba7242aa1d4450acf58afedad6500b044474c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:18:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'ගේට්වේ ප්රතිවෛරස් මෘදුකාංගය වෙබ්, FTP, තැපැල් හා වඩා පරිලෝකනය කිරීමට මධ්යම ප්රති-වයිරස එන්ජිම භාවිතා කරයි. එය ඔබේ පරිශීලකයන් වෙත ළඟා කිරීමට අවස්ථාවක් ඇත පෙර කරදරකාරී මෘදුකාංග රඳවා තබා විසින් ඔබගේ ජාල සම්බන්ධ උපකරණ ආරක්ෂා කරයි.';
+$lang['antivirus_app_name'] = 'ගේට්වේ ප්රතිවයිරස';
+$lang['antivirus_app_tooltip'] = 'එන්ක්රිප්ට් කරන ගොනු බොහෝ විට කරදරකාරී මෘදුකාංග ඉදිරිපත් කිරීම සඳහා භාවිතා වේ, එම නිසා ප්රති-වයිරස එන්ජිම හරහා ගොනු මේ ආකාරයේ ඉඩ ගැන පරෙස්සම් වන්න.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'වාරණ සංරක්ෂිත අගය අවලංගුය එන්ක්රිප්ට්.';
+$lang['antivirus_block_encrypted_files'] = 'වාරණ සංකේතාත්මක ගොනු';
+$lang['antivirus_every_two_hours'] = 'සෑම පැය දෙකකට';
+$lang['antivirus_maximum_file_size'] = 'සිප් ගොනු උපරිම ගොනු විශාලත්වය';
+$lang['antivirus_maximum_file_size_invalid'] = 'උපරිම ගොනු විශාලත්වය වලංගු නැත.';
+$lang['antivirus_maximum_files'] = 'සිප් ගොනු උපරිම ගොනු';
+$lang['antivirus_maximum_files_invalid'] = 'ගොනු උපරිම අංකය සාවද්ය වේ.';
+$lang['antivirus_maximum_recursion'] = 'සිප් ගොනු උපරිම සහානුයාත වලට';
+$lang['antivirus_maximum_recursion_invalid'] = 'උපරිම සහානුයාත වලට වලංගු නැත.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'ස්කෑන් රාජ්ය cloaking SSL වලංගු නැත.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL එක නොගැලපීම ස්කෑන් රාජ්ය වලංගු නැත.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'ස්කෑන් URL ලිපින රාජ්ය වලංගු නැත.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'තතුබාන අත්සන් රාජ්ය සාවද්ය වේ ස්කෑන්.';
+$lang['antivirus_twice_a_day'] = 'දිනකට දෙවරක්';
+$lang['antivirus_update_interval'] = 'යාවත්කාලීන පරතරය';
+$lang['antivirus_updates_per_day_invalid'] = 'දිනකට යාවත්කාලීන අංකය සාවද්ය වේ.';
