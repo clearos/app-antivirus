09571be6376666e207efb614e47d7afba41f2e92
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:13:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'App a Gateway Antivirus sèvi ak yon motè antivirus santral nan analysis sou sit wèb ★ ftp, lapòs ak plis ankò. Li pwoteje aparèy ki konekte nan rezo w lan pa rete malveyan anvan li gen yon chans yo rive jwenn itilizatè ou an.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'dosye chiffres yo souvan itilize rive delivre malveyan, kidonk, tanpri dwe fè atansyon sou sa ki pèmèt sa yo kalite dosye nan motè a antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blòk chiffres valè achiv se valab.';
+$lang['antivirus_block_encrypted_files'] = 'Blòk kripte Dosye';
+$lang['antivirus_every_two_hours'] = 'Chak èdtan de';
+$lang['antivirus_maximum_file_size'] = 'Maksimòm Gwosè dosye nan Kòd Postal Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimòm gwosè dosye se valab.';
+$lang['antivirus_maximum_files'] = 'Dosye Maksimòm nan Kòd Postal Files';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimòm kantite dosye se valab.';
+$lang['antivirus_maximum_recursion'] = 'Maksimòm Rkursyon nan Kòd Postal Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimòm rkursyon se valab.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking eta eskanè se valab.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL dezekilib eskanè Eta a se valab.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL Eta a se valab.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Èskrokri siyati analysis Eta a se valab.';
+$lang['antivirus_twice_a_day'] = 'De fwa nan yon jou';
+$lang['antivirus_update_interval'] = 'Mizajou Entèval';
+$lang['antivirus_updates_per_day_invalid'] = 'Nombre de dènye pou chak jou se valab.';
