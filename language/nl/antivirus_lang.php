56bb008d3a6f8542967c0bac15ae8e719f9d9b13
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:17:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app maakt gebruik van een centrale antivirus engine om web, FTP, e-mail en nog veel meer scannen. Het beschermt apparaten die zijn aangesloten op uw netwerk door het stoppen van malware voordat het een kans om uw gebruikers te bereiken.';
+$lang['antivirus_app_name'] = 'gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Gecodeerde bestanden worden vaak gebruikt om malware te leveren, dus wees voorzichtig over het toestaan ​​van dit soort bestanden via de antivirus engine.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok gecodeerde archief waarde is ongeldig.';
+$lang['antivirus_block_encrypted_files'] = 'Blok gecodeerde bestanden';
+$lang['antivirus_every_two_hours'] = 'Elke twee uur';
+$lang['antivirus_maximum_file_size'] = 'Maximale bestandsgrootte in Zip-bestanden';
+$lang['antivirus_maximum_file_size_invalid'] = 'De maximale bestandsgrootte is ongeldig.';
+$lang['antivirus_maximum_files'] = 'Maximum aantal bestanden in Zip-bestanden';
+$lang['antivirus_maximum_files_invalid'] = 'Maximum aantal bestanden is ongeldig.';
+$lang['antivirus_maximum_recursion'] = 'Maximum Recursion in Zip-bestanden';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximale recursie is ongeldig.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan staat is ongeldig.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch scan staat is ongeldig.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL staat is ongeldig.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing handtekeningen scan staat is ongeldig.';
+$lang['antivirus_twice_a_day'] = 'Tweemaaldaags';
+$lang['antivirus_update_interval'] = 'Bijwerkingsonderbreking';
+$lang['antivirus_updates_per_day_invalid'] = 'Aantal updates per dag is ongeldig.';
