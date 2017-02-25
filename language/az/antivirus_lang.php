df5002db49e0f1599526789e48fb099cc5c0fb35
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:10:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus app web, FTP, poçt və daha çox scan mərkəzi antivirus motor istifadə edir. Bu istifadəçi olmaq üçün bir şans var əvvəl malware dayandıraraq şəbəkə bağlı cihazlar qoruyur.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Şifrelenmiş faylları tez-tez malware çatdırmaq, belə ki, antivirus mühərriki vasitəsilə faylları bu cür imkan haqqında ehtiyatlı olun üçün istifadə olunur.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block şifrelenmiş arxiv dəyəri yanlışdır.';
+$lang['antivirus_block_encrypted_files'] = 'Block Şifrelenmiş Files';
+$lang['antivirus_every_two_hours'] = 'Hər iki saat';
+$lang['antivirus_maximum_file_size'] = 'Zip Files maksimum fayl ölçüsü';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimum fayl ölçüsü yanlışdır.';
+$lang['antivirus_maximum_files'] = 'Zip Files Maximum Files';
+$lang['antivirus_maximum_files_invalid'] = 'faylları maksimal sayı yanlışdır.';
+$lang['antivirus_maximum_recursion'] = 'Zip Files Maximum Recursion';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimum recursion yanlışdır.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL gizleme scan dövlət yanlışdır.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL uyğunsuzluq scan dövlət yanlışdır.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL&#39;leri dövlət yanlışdır.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'imza dövlət scan Phishing yanlışdır.';
+$lang['antivirus_twice_a_day'] = 'Gündə iki dəfə';
+$lang['antivirus_update_interval'] = 'Update Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'gündə yenilikləri sayı yanlışdır.';
