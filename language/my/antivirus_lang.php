89855c76a541b112d5f72ff8f182c6885645ca23
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:11:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'အဆိုပါ Gateway ရဲ့ Antivirus ကို app ကိုဝက်ဘ်, FTP, mail နဲ့ပိုပြီး scan တစ်ခုအလယ်ပိုင်း antivirus ကိုအင်ဂျင်ကိုအသုံးပြုပါတယ်။ ဒါဟာသင့်ရဲ့အသုံးပြုသူများကိုရောက်ရှိရန်အခွင့်အလမ်းရှိပါတယ်မီက malware ကိုရပ်တန့်ခြင်းအားဖြင့်သင့် network ကိုချိတ်ဆက် devices များကိုကာကွယ်ပေးသည်။';
+$lang['antivirus_app_name'] = 'gateway မှာ Antivirus';
+$lang['antivirus_app_tooltip'] = 'စာဝှက်ထားတဲ့ဖိုင်တွေမကြာခဏ malware ကိုကယ်နှုတ်ဖို့အသုံးပြု, ဒါကြောင့် antivirus ကိုအင်ဂျင်ကတဆင့်ဖိုင်များကိုဤအမျိုးအစားများကိုခွင့်ပြု ပတ်သက်. သတိပြုရကြမည်ကျေးဇူးတင်ကြသည်။';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'block archive ကိုတန်ဖိုးသည်မမှန်ကန်ပါ encrypt လုပ်ထားတဲ့။';
+$lang['antivirus_block_encrypted_files'] = 'Block ကို Encrypted Files';
+$lang['antivirus_every_two_hours'] = 'တိုင်းနှစ်ဦးအချိန်နာရီ';
+$lang['antivirus_maximum_file_size'] = 'Zip Files တွေကိုထဲမှာအများဆုံးဖိုင် Size ကို';
+$lang['antivirus_maximum_file_size_invalid'] = 'အများဆုံးဖိုင်အရွယ်အစားဟာမမှန်ကန်ဘူး။';
+$lang['antivirus_maximum_files'] = 'Zip Files တွေကိုထဲမှာအများဆုံး Files တွေကို';
+$lang['antivirus_maximum_files_invalid'] = 'ဖိုင်တွေရဲ့အမြင့်ဆုံးအရေအတွက်မမှန်ကန်ဘူး။';
+$lang['antivirus_maximum_recursion'] = 'Zip Files တွေကိုထဲမှာအများဆုံး request ကို';
+$lang['antivirus_maximum_recursion_invalid'] = 'အများဆုံး request ကိုမမှန်ကန်ဘူး။';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'scan ကပြည်နယ် cloaking SSL ကမမှန်ကန်ဘူး။';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL က URL ကိုမတိုက်ဆိုင် scan ကပြည်နယ်မမှန်ကန်ဘူး။';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan ကို URL များကိုပြည်နယ်မမှန်ကန်ဘူး။';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'phishing လက်မှတ်ပေါင်းပြည်နယ်မမှန်ကန် scan ။';
+$lang['antivirus_twice_a_day'] = 'နှစ်ကြိမ်နေ့';
+$lang['antivirus_update_interval'] = 'Update ကို Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'တစ်နေ့လျှင် updates များကိုအရေအတွက်မမှန်ကန်ဘူး။';
