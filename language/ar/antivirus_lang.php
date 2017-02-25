f10491efa9c2d0b0a28b664272c24350bc89be31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:10:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'يستخدم التطبيق بوابة مكافحة الفيروسات محرك مكافحة الفيروسات المركزي لمسح على شبكة الإنترنت، بروتوكول نقل الملفات، والبريد، وأكثر من ذلك. لأنه يحمي الأجهزة المتصلة بالشبكة من خلال وقف البرمجيات الخبيثة قبل أن تتاح لها فرصة للوصول إلى المستخدمين لديك.';
+$lang['antivirus_app_name'] = 'بوابة مكافحة الفيروسات';
+$lang['antivirus_app_tooltip'] = 'وغالبا ما تستخدم الملفات المشفرة لتقديم البرامج الضارة، لذلك يرجى توخي الحذر حول السماح لهذه الأنواع من الملفات من خلال محرك مكافحة الفيروسات.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'كتلة المشفرة قيمة أرشيف غير صالحة.';
+$lang['antivirus_block_encrypted_files'] = 'الملفات المشفرة كتلة';
+$lang['antivirus_every_two_hours'] = 'كل ساعتين';
+$lang['antivirus_maximum_file_size'] = 'حجم الملف الأقصى في ملفات مضغوطة';
+$lang['antivirus_maximum_file_size_invalid'] = 'الحد الأقصى لحجم الملف غير صالح.';
+$lang['antivirus_maximum_files'] = 'الملفات القصوى في ملفات مضغوطة';
+$lang['antivirus_maximum_files_invalid'] = 'الحد الأقصى لعدد الملفات غير صالح.';
+$lang['antivirus_maximum_recursion'] = 'العودية القصوى في ملفات مضغوطة';
+$lang['antivirus_maximum_recursion_invalid'] = 'العودية القصوى غير صالحة.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL الدولة تفحص الحجب غير صالحة.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'عدم تطابق URL SSL الدولة تفحص غير صالحة.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'الدولة مسح عناوين المواقع غير صالحة.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'التصيد التوقيعات مسح دولة غير صالح.';
+$lang['antivirus_twice_a_day'] = 'مرتين فى اليوم';
+$lang['antivirus_update_interval'] = 'الفاصل الزمني للتحديث';
+$lang['antivirus_updates_per_day_invalid'] = 'عدد من التحديثات في يوم غير صالح.';
