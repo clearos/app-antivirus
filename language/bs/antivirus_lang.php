2b11d79d545a55a6485c9561116e316606e515fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:11:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus aplikacija koristi centralnu antivirusni motor za skeniranje web, FTP, pošte i još mnogo toga. Štiti uređaji povezani na mrežu zaustavljanjem malware prije nego što ima priliku da ostvarite svoje korisnike.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Šifrirani datoteke se često koriste za isporuku malware, pa budite oprezni o omogućavajući ove vrste datoteka putem antivirus motor.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok šifrirane arhiva vrijednost je nevažeća.';
+$lang['antivirus_block_encrypted_files'] = 'Blok Šifrirani Files';
+$lang['antivirus_every_two_hours'] = 'Svaka dva sata';
+$lang['antivirus_maximum_file_size'] = 'Maksimalna veličina datoteke u ZIP datoteke';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimalna veličina datoteke je nevažeći.';
+$lang['antivirus_maximum_files'] = 'Maksimalna datoteke u ZIP datoteke';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimalan broj datoteka je nevažeća.';
+$lang['antivirus_maximum_recursion'] = 'Maksimalna rekurzije u ZIP datoteke';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimalna rekurzije je nevažeća.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL skrivanje država skeniranje je nevažeća.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL neusklađenost državnih skeniranje je nevažeća.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL-ova država je nevažeća.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing potpisa skeniranje stanje je nevažeća.';
+$lang['antivirus_twice_a_day'] = 'Dva puta dnevno';
+$lang['antivirus_update_interval'] = 'Update Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Broj ažuriranja dnevno je nevažeća.';
