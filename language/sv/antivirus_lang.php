329d71dce91104e00f2859788f63ffb735e5fc15
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:20:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus app använder en central antivirusmotor för att skanna webben, FTP, e-post och mer. Det skyddar enheter som är anslutna till nätverket genom att stoppa skadlig kod innan den har en chans att nå dina användare.';
+$lang['antivirus_app_name'] = 'gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Krypterade filer används ofta för att leverera skadlig kod, så var försiktig om att låta dessa typer av filer genom antivirusmotorn.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block krypterat arkiv värde är ogiltigt.';
+$lang['antivirus_block_encrypted_files'] = 'Block krypterade filer';
+$lang['antivirus_every_two_hours'] = 'Varannan timme';
+$lang['antivirus_maximum_file_size'] = 'Maximal filstorlek i zip-filer';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maximal filstorlek är ogiltig.';
+$lang['antivirus_maximum_files'] = 'Max antal filer i zip-filer';
+$lang['antivirus_maximum_files_invalid'] = 'Maximalt antal filer är ogiltig.';
+$lang['antivirus_maximum_recursion'] = 'Maximal rekursion i zip-filer';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximal rekursion är ogiltig.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan tillstånd är ogiltigt.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'mismatch SSL URL scan tillstånd är ogiltigt.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan webbadresser tillstånd är ogiltigt.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing signaturer skanna tillstånd är ogiltigt.';
+$lang['antivirus_twice_a_day'] = 'Två gånger om dagen';
+$lang['antivirus_update_interval'] = 'uppdaterings~~POS=TRUNC';
+$lang['antivirus_updates_per_day_invalid'] = 'Antal uppdateringar per dag är ogiltig.';
