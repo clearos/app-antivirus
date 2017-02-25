c0e65a875c7e60a9a4af18881146c23f329bce8b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:15:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = '게이트웨이 안티 바이러스 응용 프로그램은 웹, FTP, 메일 등을 스캔 중앙 안티 바이러스 엔진을 사용합니다. 그것은 사용자에 도달 할 수있는 기회가되기 전에 악성 코드를 중지하여 네트워크에 연결된 장치를 보호합니다.';
+$lang['antivirus_app_name'] = '게이트웨이 안티 바이러스';
+$lang['antivirus_app_tooltip'] = '암호화 된 파일은 종종 악성 코드를 제공하기 때문에 바이러스 백신 엔진을 통해 이러한 형식의 파일을 허용 조심해주세요하는 데 사용됩니다.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = '블록 암호화 된 아카이브 값이 잘못되었습니다.';
+$lang['antivirus_block_encrypted_files'] = '블록 암호화 된 파일';
+$lang['antivirus_every_two_hours'] = '두 시간마다';
+$lang['antivirus_maximum_file_size'] = '우편 번호 파일의 최대 파일 크기';
+$lang['antivirus_maximum_file_size_invalid'] = '최대 파일 크기가 잘못되었습니다.';
+$lang['antivirus_maximum_files'] = '우편 번호 파일의 최대 파일';
+$lang['antivirus_maximum_files_invalid'] = '파일의 최대 개수가 잘못되었습니다.';
+$lang['antivirus_maximum_recursion'] = '우편 번호 파일의 최대 재귀';
+$lang['antivirus_maximum_recursion_invalid'] = '최대 재귀가 잘못되었습니다.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL 클로킹 스캔 상태가 잘못되었습니다.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL의 URL이 일치 스캔 상태가 잘못되었습니다.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = '스캔 된 URL 상태가 잘못되었습니다.';
+$lang['antivirus_phishing_signatures_state_invalid'] = '서명 상태를 검사 피싱 유효하지 않습니다.';
+$lang['antivirus_twice_a_day'] = '하루에 두번';
+$lang['antivirus_update_interval'] = '업데이트 간격';
+$lang['antivirus_updates_per_day_invalid'] = '하루에 업데이트 번호가 잘못되었습니다.';
