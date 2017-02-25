01cc85ff7f49657939e3c28b495fa8b9bcad1b6e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:16:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'апликација Портал антивирус користи централна антивирус моторот за скенирање на веб, FTP, пошта и повеќе. Таа го заштитува уреди поврзани на вашата мрежа преку запирање на малициозен софтвер пред да има шанса да стигнат до вашите корисници.';
+$lang['antivirus_app_name'] = 'портал антивирус';
+$lang['antivirus_app_tooltip'] = 'Криптирани датотеки, често се користи за реализација на малициозен софтвер, па ве молиме да се биде внимателен во врска со дозволувајќи им на овие видови на фајлови преку антивирусен моторот.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Забрани шифрирана архива вредноста не е валиден.';
+$lang['antivirus_block_encrypted_files'] = 'Забрани криптирани датотеки';
+$lang['antivirus_every_two_hours'] = 'На секои два часа';
+$lang['antivirus_maximum_file_size'] = 'Максимална големина на датотеки во zip датотеки';
+$lang['antivirus_maximum_file_size_invalid'] = 'Максимална големина на датотеката не е валидна.';
+$lang['antivirus_maximum_files'] = 'Максимална датотеки во zip датотеки';
+$lang['antivirus_maximum_files_invalid'] = 'Максималниот број на датотеки не е валиден.';
+$lang['antivirus_maximum_recursion'] = 'Максимална Рекурзија во Zip датотеки';
+$lang['antivirus_maximum_recursion_invalid'] = 'Максимална рекурзијата е валиден.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL поезијата државата скенирање не е валиден.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL Несогласување скенирање држава е валиден.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Скенирајте адреси држава е валиден.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing потписи скенирање држава е валиден.';
+$lang['antivirus_twice_a_day'] = 'Два пати на ден';
+$lang['antivirus_update_interval'] = 'Update interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Број на надградби на ден не е валиден.';
