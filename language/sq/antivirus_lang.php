829df3718da4263ad95984a59a82e5ca5a371016
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:10:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'App Gateway Antivirus përdor një motor antivirus qendror të scan web, FTP, postën dhe më shumë. Ajo mbron pajisjet e lidhura me rrjetin tuaj duke ndalur malware para se ai ka një shans për të arritur përdoruesit tuaj.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Encrypted fotografi janë përdorur shpesh për të ofruar malware, pra ju lutemi të jenë të kujdesshëm në lidhje me lejimin e këtyre llojeve të dosjeve nëpër motor antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block vlera arkiv koduar është e pavlefshme.';
+$lang['antivirus_block_encrypted_files'] = 'Block Encrypted Files';
+$lang['antivirus_every_two_hours'] = 'Çdo dy orë';
+$lang['antivirus_maximum_file_size'] = 'Maximum Madhësia e dokumentit në Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Madhësia maksimale e skedarit është i pavlefshëm.';
+$lang['antivirus_maximum_files'] = 'Files maksimale në Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Numri maksimal i dosjeve është e pavlefshme.';
+$lang['antivirus_maximum_recursion'] = 'Maximum Recursion në Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion maksimale është e pavlefshme.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL shteti cloaking scan është i pavlefshëm.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mospërputhje scan shteti është i pavlefshëm.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Shteti Scan URL është i pavlefshëm.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing nënshkrimet scan shteti është i pavlefshëm.';
+$lang['antivirus_twice_a_day'] = 'Dy herë në ditë';
+$lang['antivirus_update_interval'] = 'Update Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Numri i përditësime në ditë është e pavlefshme.';
