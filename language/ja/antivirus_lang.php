5828c64168ade28ae64daad33432583141abcac2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:14:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'ゲートウェイアンチウィルスアプリは、Web、FTP、メールなどをスキャンするために中央のウイルス対策エンジンを使用しています。 それはあなたのユーザーに到達する機会を持つ前にマルウェアを停止することにより、ネットワークに接続された機器を保護します。';
+$lang['antivirus_app_name'] = 'ゲートウェイアンチウイルス';
+$lang['antivirus_app_tooltip'] = '暗号化されたファイルは、多くの場合、マルウェアを配信するために使用するので、ウイルス対策エンジンを介してこれらのタイプのファイルを可能にすることに注意してくださいされています。';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'ブロック暗号化されたアーカイブ値が無効です。';
+$lang['antivirus_block_encrypted_files'] = 'ブロック暗号化ファイル';
+$lang['antivirus_every_two_hours'] = '2時間ごとに';
+$lang['antivirus_maximum_file_size'] = 'ZIPファイルの最大ファイルサイズ';
+$lang['antivirus_maximum_file_size_invalid'] = '最大ファイルサイズは無効です。';
+$lang['antivirus_maximum_files'] = 'ZIPファイルの最大ファイル';
+$lang['antivirus_maximum_files_invalid'] = 'ファイルの最大数が無効です。';
+$lang['antivirus_maximum_recursion'] = 'ZIPファイルの最大再帰';
+$lang['antivirus_maximum_recursion_invalid'] = '最大の再帰は無効です。';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSLクローキングスキャン状態が無効です。';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSLのURLの不一致スキャン状態が無効です。';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'スキャンURLの状態が無効です。';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'フィッシング署名は状態が無効であるスキャンします。';
+$lang['antivirus_twice_a_day'] = '1日2回';
+$lang['antivirus_update_interval'] = '更新間隔';
+$lang['antivirus_updates_per_day_invalid'] = '一日あたりの更新回数が無効です。';
