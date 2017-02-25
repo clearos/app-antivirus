6ab514e446c5afc4f0fa8afa479a55ada48fb8c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:15:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'កម្មវិធីប្រឆាំងមេរោគផ្លូវចេញចូលម៉ាស៊ីនកំចាត់មេរោគមួយដែលប្រើដើម្បីវិភាគរកបណ្ដាញភាគកណ្តាល, FTP របស់សំបុត្រនិងច្រើនទៀត។ វាការពារឧបករណ៍ភ្ជាប់បណ្តាញរបស់អ្នកដោយការបញ្ឈប់មេរោគមុនពេលវាមានឱកាសក្នុងការឈានទៅដល់អ្នកប្រើប្រាស់របស់អ្នក។';
+$lang['antivirus_app_name'] = 'ផ្លូវចេញចូលប្រឆាំងមេរោគ';
+$lang['antivirus_app_tooltip'] = 'ឯកសារដែលបានអ៊ិនគ្រីបជាញឹកញាប់ត្រូវបានគេប្រើដើម្បីបញ្ជូនមេរោគ, ដូច្នេះសូមមានការប្រុងប្រយ័ត្នអំពីការអនុញ្ញាតឱ្យប្រភេទនៃឯកសារទាំងនេះតាមរយៈម៉ាស៊ីនកំចាត់មេរោគនេះ។';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'បណ្ណសារដែលបានអ៊ិនគ្រីបប្លុកតម្លៃគឺមិនត្រឹមត្រូវ។';
+$lang['antivirus_block_encrypted_files'] = 'ប្លុកអ៊ិនគ្រីបឯកសារ';
+$lang['antivirus_every_two_hours'] = 'រៀងរាល់ពីរម៉ោង';
+$lang['antivirus_maximum_file_size'] = 'ទំហំឯកសារអតិបរមាក្នុងឯកសារ zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'ទំហំឯកសារអតិបរមាគឺមិនត្រឹមត្រូវ។';
+$lang['antivirus_maximum_files'] = 'ឯកសារអតិបរមាក្នុងឯកសារ zip';
+$lang['antivirus_maximum_files_invalid'] = 'ចំនួនអតិបរមានៃឯកសារមិនត្រឹមត្រូវ។';
+$lang['antivirus_maximum_recursion'] = 'អតិបរមាហៅខ្លួនឯងនៅក្នុងឯកសារ zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'ការហៅខ្លួនឯងអតិបរមាមិនត្រឹមត្រូវ។';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'រដ្ឋស្កេនបិទបាំង SSL គឺមិនត្រឹមត្រូវ។';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'រដ្ឋស្កេន SSL ដែលភាពខុសគ្នា URL ដែលមិនត្រឹមត្រូវ។';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'រដ្ឋវិភាគ URLs ដែលមិនត្រឹមត្រូវ។';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'បន្លំហត្ថលេខាស្កេនរដ្ឋមិនត្រឹមត្រូវ។';
+$lang['antivirus_twice_a_day'] = 'ពីរ​ដង​ក្នុងមួយថ្ងៃ';
+$lang['antivirus_update_interval'] = 'ចន្លោះពេលធ្វើឱ្យទាន់សម័យ';
+$lang['antivirus_updates_per_day_invalid'] = 'ចំនួននៃការធ្វើឱ្យទាន់សម័យក្នុងមួយថ្ងៃគឺមិនត្រឹមត្រូវ។';
