39f6a63ac43ba98feff42a377e4193a223a6fe6c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:17:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Il-app Bieb Antivirus juża magna antivirus ċentrali jiġu skennjati web, FTP, posta u aktar. Jipproteġi mezzi konnessi man-network tiegħek mill-waqfien malware qabel ma ċ-ċans biex tilħaq lill-utenti tiegħek.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'fajls encrypted ħafna drabi jintużaw biex iwasslu malware, għalhekk jekk jogħġbok tkun attenta dwar li jippermetti lil dawn it-tipi ta &#39;fajls permezz tal-magna antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blokk valur arkivju encrypted huwa invalidu.';
+$lang['antivirus_block_encrypted_files'] = 'Blokk Encrypted Fajls';
+$lang['antivirus_every_two_hours'] = 'Kull sagħtejn';
+$lang['antivirus_maximum_file_size'] = 'Massimu Daqs tal-fajl fil Zip fajls';
+$lang['antivirus_maximum_file_size_invalid'] = 'Daqs massimu tal-fajl huwa invalidu.';
+$lang['antivirus_maximum_files'] = 'Fajls massimi Zip Fajls';
+$lang['antivirus_maximum_files_invalid'] = 'Numru massimu ta &#39;fajls huwa invalidu.';
+$lang['antivirus_maximum_recursion'] = 'Massimu Recursion fil Zip fajls';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion massimu huwa invalidu.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL iskan istat cloaking hija invalida.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL nuqqas ta &#39;qbil iskan istat huwa invalidu.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'istat Scan URLs huwa invalidu.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing firem scan istat huwa invalidu.';
+$lang['antivirus_twice_a_day'] = 'Darbtejn kuljum';
+$lang['antivirus_update_interval'] = 'aġġornament Intervall';
+$lang['antivirus_updates_per_day_invalid'] = 'Numru ta &#39;aġġornamenti kuljum huwa invalidu.';
