0c6aa15b55e941d4e4d3a835cae3101ebdd1f874
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:20:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'நுழைவாயில் வைரஸ் பயன்பாட்டை வலை, FTP மின்னஞ்சல் மேலும் ஸ்கேன் ஒரு மத்திய வைரஸ் இயந்திரம் பயன்படுத்துகிறது. அது உங்கள் பயனர் அடைய ஒரு வாய்ப்பு உள்ளது முன் தீம்பொருள் நிறுத்தி உங்கள் நெட்வொர்க் இணைக்கப்பட்ட சாதனங்கள் பாதுகாக்கிறது.';
+$lang['antivirus_app_name'] = 'நுழைவாயில் வைரஸ்';
+$lang['antivirus_app_tooltip'] = 'மாற்றப்பட்ட கோப்புகளை அடிக்கடி தீம்பொருள் வழங்க, எனவே வைரஸ் இயந்திரம் மூலம் கோப்புகளை இந்த வகையான அனுமதிப்பது பற்றி கவனமாக இருக்க தயவு செய்து பயன்படுத்தப்படுகின்றன.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'பிளாக் மறைகுறியாக்கப்பட்ட காப்பகத்தை மதிப்பு தவறானது.';
+$lang['antivirus_block_encrypted_files'] = 'பிளாக் மாற்றப்பட்ட கோப்புகளை';
+$lang['antivirus_every_two_hours'] = 'ஒவ்வொரு இரண்டு மணி நேரம்';
+$lang['antivirus_maximum_file_size'] = 'Zip கோப்புகளை உள்ள அதிகபட்ச கோப்பு அளவு';
+$lang['antivirus_maximum_file_size_invalid'] = 'அதிகபட்ச கோப்பு அளவு தவறானது.';
+$lang['antivirus_maximum_files'] = 'Zip கோப்புகளை உள்ள அதிகபட்ச கோப்புகள்';
+$lang['antivirus_maximum_files_invalid'] = 'கோப்புகளை அதிகபட்ச தவறானது.';
+$lang['antivirus_maximum_recursion'] = 'Zip கோப்புகளை உள்ள அதிகபட்ச மறுசுழற்சி';
+$lang['antivirus_maximum_recursion_invalid'] = 'அதிகபட்ச மறுநிகழ்வு தவறானது.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL ஐ மண்டலத்திலிருந்து ஸ்கேன் மாநில தவறானது.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL ஐ URL ஐ பொருத்தமில்லாமல் ஸ்கேன் மாநில தவறானது.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'ஸ்கேன் URL கள் மாநில தவறானது.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'ஃபிஷிங் கையெழுத்துக்களை மாநிலம் ஸ்கேன் தவறானது.';
+$lang['antivirus_twice_a_day'] = 'ஒரு நாளுக்கு இரு தடவைகள்';
+$lang['antivirus_update_interval'] = 'மேம்படுத்தல் இடைவெளி';
+$lang['antivirus_updates_per_day_invalid'] = 'நாள் ஒன்றுக்கு மேம்படுத்தல்கள் எண் தவறானது.';
