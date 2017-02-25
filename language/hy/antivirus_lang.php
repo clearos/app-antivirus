fad83f4dd4fdbb0e17156d8c32d6834242947b3d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:10:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Հակավիրուսային ծրագիրը օգտագործում է կենտրոնական հակավիրուսային շարժիչը սկան վեբ, FTP, փոստով եւ այլն: Այն պաշտպանում է սարքերի հետ կապված ձեր ցանցին կանգնեցնելով չարամիտ, նախքան այն ունի հնարավորություն է հասնել Ձեր օգտվողներին.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Կոդավորված ֆայլեր են հաճախ օգտագործվում է մատուցել չարամիտ, ուստի խնդրում ենք լինել զգույշ մասին, թույլ տալով այդ տեսակի ֆայլեր միջոցով հակավիրուսային շարժիչը:';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Արգելափակել կոդավորված պահոց արժեքն է անվավեր:';
+$lang['antivirus_block_encrypted_files'] = 'Արգելափակել կոդավորված ֆայլեր';
+$lang['antivirus_every_two_hours'] = 'Ամեն երկու ժամ';
+$lang['antivirus_maximum_file_size'] = 'Առավելագույն Ֆայլի չափը zip ֆայլեր';
+$lang['antivirus_maximum_file_size_invalid'] = 'Ֆայլի առավելագույն չափը անվավեր:';
+$lang['antivirus_maximum_files'] = 'Առավելագույն Ֆայլերը zip ֆայլեր';
+$lang['antivirus_maximum_files_invalid'] = 'Առավելագույն թվով ֆայլերի անվավեր է:';
+$lang['antivirus_maximum_recursion'] = 'Առավելագույն Ռեկուրսիա է Zip Ֆայլեր';
+$lang['antivirus_maximum_recursion_invalid'] = 'Առավելագույն Ռեկուրսիա է անվավեր:';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking փնտրել պետությունն է անվավեր:';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL անհամապատասխանություն փնտրել պետությունն է անվավեր:';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Փնտրել ինտերնետ հասցեներ պետությունն է անվավեր:';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Խայծ ստորագրությունները սկան պետություն է անվավեր:';
+$lang['antivirus_twice_a_day'] = 'Երկու անգամ մի օր';
+$lang['antivirus_update_interval'] = 'Թարմացնել Ընդմիջումից';
+$lang['antivirus_updates_per_day_invalid'] = 'Միավորների քան-թարմացումների մեկ օրում անվավեր է.';
