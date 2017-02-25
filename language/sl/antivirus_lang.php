60584536e29473d3d2da4f1dd3470f8b29e008dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:19:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus aplikacija uporablja osrednjo protivirusen orodje za skeniranje spletnega, FTP, pošto in več. Ščiti naprave, povezane z omrežjem, ki ga ustavil zlonamerne programske opreme, preden se je priložnost, da dosežejo svoje uporabnike.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Šifrirane datoteke se pogosto uporabljajo za zagotavljanje zlonamerne programske opreme, zato bodite previdni, kar te vrste datotek s protivirusno motorja.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block šifrirano arhiv vrednost je neveljavna.';
+$lang['antivirus_block_encrypted_files'] = 'Blok šifriranih datotek';
+$lang['antivirus_every_two_hours'] = 'Vsaki dve uri';
+$lang['antivirus_maximum_file_size'] = 'Največja velikost datoteke v Zip datotek';
+$lang['antivirus_maximum_file_size_invalid'] = 'Največja velikost datoteke je neveljavna.';
+$lang['antivirus_maximum_files'] = 'Največje število datotek v Zip datotek';
+$lang['antivirus_maximum_files_invalid'] = 'Največje število datotek je neveljavna.';
+$lang['antivirus_maximum_recursion'] = 'Največja Rekurzija v Zip datotek';
+$lang['antivirus_maximum_recursion_invalid'] = 'Največja rekurzija je neveljavna.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL skrivanje skeniranje stanje je neveljavna.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL neusklajenost skeniranje stanje je neveljavna.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL stanje je neveljavna.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing podpisi skeniranje stanje je neveljavna.';
+$lang['antivirus_twice_a_day'] = 'Dvakrat na dan';
+$lang['antivirus_update_interval'] = 'Update Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Število posodobitve na dan, je neveljavna.';
