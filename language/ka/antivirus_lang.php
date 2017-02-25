c4d27c02957308915a4d8c80ecf573f0a340d743
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:14:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'კარიბჭე Antivirus app იყენებს ცენტრალური ანტივირუსული ძრავის სკანირების ვებ, FTP, ფოსტა და სხვა. ეს იცავს მოწყობილობების დაკავშირებული თქვენი ქსელის მიერ შეჩერების malware სანამ აქვს შანსი მიაღწიოს თქვენი მომხმარებლები.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'დაშიფრული ფაილი ხშირად გამოიყენება სიტყვით malware, ასე რომ გთხოვთ ფრთხილად საშუალებას ამ ტიპის ფაილი მეშვეობით ანტივირუსული ძრავა.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block დაშიფრული არქივი მნიშვნელობა არასწორია.';
+$lang['antivirus_block_encrypted_files'] = 'Block დაშიფრული ფაილები';
+$lang['antivirus_every_two_hours'] = 'ყოველ ორ საათში';
+$lang['antivirus_maximum_file_size'] = 'ფაილის მაქსიმალური ზომა zip ფაილი';
+$lang['antivirus_maximum_file_size_invalid'] = 'ფაილის მაქსიმალური ზომა არასწორია.';
+$lang['antivirus_maximum_files'] = 'მაქსიმალური ფაილი Zip ფაილი';
+$lang['antivirus_maximum_files_invalid'] = 'მაქსიმალური რაოდენობა ფაილი არასწორია.';
+$lang['antivirus_maximum_recursion'] = 'მაქსიმალური უკან zip ფაილი';
+$lang['antivirus_maximum_recursion_invalid'] = 'მაქსიმალური უკან არასწორია.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking სკანირების სახელმწიფო არასწორია.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL შეუსაბამობის სკანირების სახელმწიფო არასწორია.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan მისამართები სახელმწიფო არასწორია.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'თაღლითობისგან ხელმოწერა სკანირების სახელმწიფო არასწორია.';
+$lang['antivirus_twice_a_day'] = 'დღეში ორჯერ';
+$lang['antivirus_update_interval'] = 'განახლების ინტერვალი';
+$lang['antivirus_updates_per_day_invalid'] = 'პუნქტების განახლებები დღეში არასწორია.';
