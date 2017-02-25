0c85d5a26e5f4e3affe9307ec808d6659e492c65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:20:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app ay gumagamit ng isang sentral na antivirus engine upang i-scan web, FTP, mail at higit pa. Ito pinoprotektahan device na nakakonekta sa iyong network sa pamamagitan ng paghinto malware bago ito ay may pagkakataon na maabot ang iyong mga gumagamit.';
+$lang['antivirus_app_name'] = 'gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Naka-encrypt na file ay madalas na ginagamit upang maghatid ng malware, kaya mangyaring maging maingat tungkol sa na nagpapahintulot sa mga uri ng mga file sa pamamagitan ng antivirus engine.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'I-block ang naka-encrypt na halaga archive ay hindi wasto.';
+$lang['antivirus_block_encrypted_files'] = 'Block Naka-encrypt na File';
+$lang['antivirus_every_two_hours'] = 'Bawat Dalawang Oras';
+$lang['antivirus_maximum_file_size'] = 'Maximum File Size sa Zip File';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maximum na laki ng file ay hindi wasto.';
+$lang['antivirus_maximum_files'] = 'Maximum Files sa Zip File';
+$lang['antivirus_maximum_files_invalid'] = 'Maximum na bilang ng mga file ay hindi wasto.';
+$lang['antivirus_maximum_recursion'] = 'Maximum Recursion sa Zip File';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximum recursion ay hindi wasto.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan ng estado ay hindi wasto.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch scan ng estado ay hindi wasto.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL estado ay hindi wasto.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing na mga lagda scan ng estado ay hindi wasto.';
+$lang['antivirus_twice_a_day'] = 'Dalawang beses sa isang araw';
+$lang['antivirus_update_interval'] = 'update Palugit';
+$lang['antivirus_updates_per_day_invalid'] = 'Bilang ng mga update sa bawat araw ay hindi wasto.';
