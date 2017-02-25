af76fd843d5c7e4eb2b1a79e469f3a8f0afb7002
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:21:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Mae&#39;r app Porth Antivirus defnyddio peiriant antivirus ganolog i sganio y we, FTP, post a mwy. Mae&#39;n amddiffyn dyfeisiau sy&#39;n gysylltiedig â&#39;ch rhwydwaith trwy atal malware cyn iddo gael cyfle i gyrraedd eich defnyddwyr.';
+$lang['antivirus_app_name'] = 'Porth Antivirus';
+$lang['antivirus_app_tooltip'] = 'amgryptio ffeiliau yn cael eu defnyddio&#39;n aml i gyflwyno malware, felly os gwelwch yn dda fod yn ofalus ynghylch caniatáu mathau hyn o ffeiliau drwy&#39;r peiriant antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloc gwerth archif amgryptio yn annilys.';
+$lang['antivirus_block_encrypted_files'] = 'Bloc Ffeiliau Amgryptio';
+$lang['antivirus_every_two_hours'] = 'Bob Dwy Awr';
+$lang['antivirus_maximum_file_size'] = 'Uchafswm Maint Ffeil mewn Zip Ffeiliau';
+$lang['antivirus_maximum_file_size_invalid'] = 'maint y ffeil Uchafswm yn annilys.';
+$lang['antivirus_maximum_files'] = 'Ffeiliau Uchafswm mewn Zip Ffeiliau';
+$lang['antivirus_maximum_files_invalid'] = 'Uchafswm nifer o ffeiliau yn annilys.';
+$lang['antivirus_maximum_recursion'] = 'Uchafswm recursion yn Zip Ffeiliau';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion Uchafswm yn annilys.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cyflwr sgan cloaking yn annilys.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL camgymhariad wladwriaeth sgan yn annilys.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLs wladwriaeth yn annilys.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Gwe-rwydo llofnodion sganio wladwriaeth yn annilys.';
+$lang['antivirus_twice_a_day'] = 'Dwywaith y Dydd';
+$lang['antivirus_update_interval'] = 'Diweddariad Ysbaid';
+$lang['antivirus_updates_per_day_invalid'] = 'Nifer o ddiweddariadau bob dydd yn annilys.';
