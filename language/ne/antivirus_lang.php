888892efc96583bf6f39a06b56ea92e3ba29d243
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:17:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'गेटवे एन्टिभाइरस अनुप्रयोग वेब, एफटीपी, मेल र थप स्क्यान गर्न एक केन्द्रीय एन्टिभाइरस इन्जिन प्रयोग गर्दछ। यो आफ्नो प्रयोगकर्ता पुग्न एक मौका छ अघि मालवेयर रोक गरेर आफ्नो नेटवर्कमा जडान उपकरणहरू सुरक्षा।';
+$lang['antivirus_app_name'] = 'गेटवे एन्टिभाइरस';
+$lang['antivirus_app_tooltip'] = 'गुप्तिकरणगरिएकोफाइलकालागि अक्सर मालवेयर उद्धार गर्न, त्यसैले कृपया एन्टिभाइरस इन्जिन मार्फत फाइलहरू यी प्रकारका अनुमति होसियार प्रयोग गरिन्छ।';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'ब्लक गुप्तिकरण संग्रह मान अमान्य छ।';
+$lang['antivirus_block_encrypted_files'] = 'ब्लक गुप्तिकरणगरिएकोफाइलकालागि';
+$lang['antivirus_every_two_hours'] = 'हरेक दुई घण्टा';
+$lang['antivirus_maximum_file_size'] = 'जिप फाइल अधिकतम फाइल साइज';
+$lang['antivirus_maximum_file_size_invalid'] = 'अधिकतम फाइल साइज अमान्य छ।';
+$lang['antivirus_maximum_files'] = 'जिप फाइल अधिकतम फाइल';
+$lang['antivirus_maximum_files_invalid'] = 'फाइलहरू अधिकतम संख्या अमान्य छ।';
+$lang['antivirus_maximum_recursion'] = 'जिप फाइल अधिकतम Recursion';
+$lang['antivirus_maximum_recursion_invalid'] = 'अधिकतम recursion अमान्य छ।';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL क्लोकिङ स्क्यान राज्य अमान्य छ।';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL बेमेल स्क्यान राज्य अमान्य छ।';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'स्क्यान URL हरू राज्य अमान्य छ।';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'हस्ताक्षर राज्य स्क्यान फिसिङ अमान्य छ।';
+$lang['antivirus_twice_a_day'] = 'दिनको दुई पटक';
+$lang['antivirus_update_interval'] = 'अन्तरालअद्यावधिकगर्नुहोस्';
+$lang['antivirus_updates_per_day_invalid'] = 'प्रति दिन अद्यावधिक नम्बर अवैध छ।';
