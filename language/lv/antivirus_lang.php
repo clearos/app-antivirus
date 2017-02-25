6bf39c24fbdc377c91bb60c787672e40e3ac46e0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:16:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus lietotne izmanto centrālā antivīrusu dzinēju, lai skenētu mājas, FTP, pasta un vairāk. Tas aizsargā ierīces, kas saistīts ar tīklu, pārtraucot malware, pirms tā ir iespēja sasniegt savus lietotājus.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Šifrētus failus bieži tiek izmantoti, lai nodrošinātu ļaunprātīgu programmatūru, tāpēc, lūdzu, esiet uzmanīgi par ļaujot šos failu tipus, izmantojot antivīrusu dzinējs.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block šifrētu arhīvu vērtība nav derīga.';
+$lang['antivirus_block_encrypted_files'] = 'Block šifrētu failus';
+$lang['antivirus_every_two_hours'] = 'Ik pēc divām stundām';
+$lang['antivirus_maximum_file_size'] = 'Maksimālais faila izmērs Zip faili';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimālais faila lielums ir nederīgs.';
+$lang['antivirus_maximum_files'] = 'Maksimāli Faili Zip failus';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimālais failu skaits ir nederīgs.';
+$lang['antivirus_maximum_recursion'] = 'Maksimālais Recursion Zip faili';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimālais rekursijas ir nederīgs.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan stāvoklis ir nederīgs.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL neatbilstība skenēšanas stāvoklis ir nederīgs.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL stāvoklis ir nederīgs.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Pikšķerēšanas paraksti skenēt stāvokli ir nederīgs.';
+$lang['antivirus_twice_a_day'] = 'Divas reizes dienā';
+$lang['antivirus_update_interval'] = 'Update intervāls';
+$lang['antivirus_updates_per_day_invalid'] = 'Skaits par jaunumiem dienā ir nederīgs.';
