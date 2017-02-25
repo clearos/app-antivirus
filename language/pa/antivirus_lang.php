4821c9a0638d26d82bd9f6f6cd33a28d3f1b12da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:17:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'ਗੇਟਵੇ ਐਨਟਿਵ਼ਾਇਰਅਸ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਵੈੱਬ, FTP, ਮੇਲ ਅਤੇ ਹੋਰ ਨੂੰ ਸਕੈਨ ਕਰਨ ਲਈ ਇੱਕ ਮੱਧ ਐਨਟਿਵ਼ਾਇਰਅਸ ਇੰਜਣ ਵਰਤਦਾ ਹੈ. ਇਸ ਨੂੰ ਮਾਲਵੇਅਰ ਰੋਕਣ ਦੇ ਅੱਗੇ ਇਸ ਨੂੰ ਆਪਣੇ ਉਪਭੋਗੀ ਨੂੰ ਹਾਸਲ ਕਰਨ ਲਈ ਇੱਕ ਮੌਕਾ ਹੈ ਕੇ ਆਪਣੇ ਨੈੱਟਵਰਕ ਨਾਲ ਜੁੜੇ ਜੰਤਰ ਦੀ ਰੱਖਿਆ ਕਰਦਾ ਹੈ.';
+$lang['antivirus_app_name'] = 'ਗੇਟਵੇ ਐਨਟਿਵ਼ਾਇਰਅਸ';
+$lang['antivirus_app_tooltip'] = 'ਇਨਕ੍ਰਿਪਟਡ ਫਾਇਲ ਅਕਸਰ ਮਾਲਵੇਅਰ ਨੂੰ ਬਚਾਉਣ ਲਈ ਹੈ, ਇਸ ਲਈ ਕਿਰਪਾ ਕਰਕੇ ਐਨਟਿਵ਼ਾਇਰਅਸ ਇੰਜਣ ਦੁਆਰਾ ਫਾਇਲ ਦੀ ਇਹ ਕਿਸਮ, ਜਿਸ ਬਾਰੇ ਸਾਵਧਾਨ ਹੋ ਲਈ ਵਰਤਿਆ ਜਾਦਾ ਹੈ.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'ਬਲਾਕ ਇਨਕ੍ਰਿਪਟਡ ਅਕਾਇਵ ਮੁੱਲ ਗਲਤ ਹੈ.';
+$lang['antivirus_block_encrypted_files'] = 'ਬਲਾਕ ਇੰਕ੍ਰਿਪਟਡ ਫਾਇਲ';
+$lang['antivirus_every_two_hours'] = 'ਹਰ ਦੋ ਘੰਟੇ';
+$lang['antivirus_maximum_file_size'] = 'ਜ਼ਿੱਪ ਫਾਇਲ ਵਿੱਚ ਵੱਧ ਫਾਇਲ ਆਕਾਰ';
+$lang['antivirus_maximum_file_size_invalid'] = 'ਵੱਧ ਫਾਇਲ ਅਕਾਰ ਗਲਤ ਹੈ.';
+$lang['antivirus_maximum_files'] = 'ਜ਼ਿੱਪ ਫਾਇਲ ਵਿੱਚ ਵੱਧ ਫਾਇਲ';
+$lang['antivirus_maximum_files_invalid'] = 'ਫਾਇਲ ਦੀ ਵੱਧ ਗਿਣਤੀ ਗਲਤ ਹੈ.';
+$lang['antivirus_maximum_recursion'] = 'ਜ਼ਿੱਪ ਫਾਇਲ ਵਿੱਚ ਵੱਧ ਲੜੀ';
+$lang['antivirus_maximum_recursion_invalid'] = 'ਵੱਧ ਲੜੀ ਗਲਤ ਹੈ.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL ਨੂੰ cloaking ਸਕੈਨ ਰਾਜ ਦੇ ਗਲਤ ਹੈ.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL ਨੂੰ URL ਨੂੰ ਮੇਲ ਸਕੈਨ ਰਾਜ ਦੇ ਗਲਤ ਹੈ.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'ਸਕੈਨ URL ਨੂੰ ਰਾਜ ਦੇ ਗਲਤ ਹੈ.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'ਫਿਸ਼ਿੰਗ ਦਸਤਖਤ ਰਾਜ ਦੇ ਸਕੈਨ ਗਲਤ ਹੈ.';
+$lang['antivirus_twice_a_day'] = 'ਦਿਨ ਵਿਚ ਦੋ ਵਾਰ';
+$lang['antivirus_update_interval'] = 'ਅੱਪਡੇਟ ਅੰਤਰਾਲ';
+$lang['antivirus_updates_per_day_invalid'] = 'ਪ੍ਰਤੀ ਦਿਨ ਅੱਪਡੇਟ ਦੀ ਗਿਣਤੀ ਗਲਤ ਹੈ.';
