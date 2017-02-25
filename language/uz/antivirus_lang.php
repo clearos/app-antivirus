8573afe8e6775f0d54402c7551a93a32ec2bb404
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:21:20 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway antivirus ilova veb-, FTP, pochta va yana harakatlanish uchun markaziy antivirus qidiruvi foydalanadi. Bu sizning foydalanuvchilar erishish uchun imkoniyat bor oldin malware to&#39;xtatish orqali tarmoqqa ulangan qurilmalar himoya qiladi.';
+$lang['antivirus_app_name'] = 'Gateway antivirus';
+$lang['antivirus_app_tooltip'] = 'Shifrlangan fayllar ko&#39;pincha zararli dastur yetkazib, shuning uchun virusga qarshi behosdan fayllar Ushbu turdagi ruxsat beruvchi haqida ehtiyot bo&#39;lish iltimos uchun ishlatiladi.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok shifrlangan arxiv qiymati yaroqsiz.';
+$lang['antivirus_block_encrypted_files'] = 'Blok shifrlangan fayllar';
+$lang['antivirus_every_two_hours'] = 'Har ikki soat';
+$lang['antivirus_maximum_file_size'] = 'Zip fayl maksimal fayl hajmi';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimal Fayl hajmi haqiqiy emas.';
+$lang['antivirus_maximum_files'] = 'Zip fayl maksimal Fayllar';
+$lang['antivirus_maximum_files_invalid'] = 'fayllar maksimal soni haqiqiy emas.';
+$lang['antivirus_maximum_recursion'] = 'Zip fayl maksimal Mavzu bo&#39;yicha';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimal yineleme haqiqiy emas.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL bekitish, ko&#39;rish davlat yaroqsiz hisoblanadi.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL uyumsuzluğu scan davlat haqiqiy emas.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL davlat haqiqiy emas.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'imzo holatini to&#39;laligicha fishing haqiqiy emas.';
+$lang['antivirus_twice_a_day'] = 'Kuniga ikki marta';
+$lang['antivirus_update_interval'] = 'yangilash oralig&#39;i';
+$lang['antivirus_updates_per_day_invalid'] = 'kuniga yangilanishlar raqami noto&#39;g&#39;ri ko&#39;rsatilgan.';
