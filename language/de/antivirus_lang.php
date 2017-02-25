e89194e9e7c2d2f6851836fa7a5f3264b05b47f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:13:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Die Gateway-Antivirus-App nutzt eine zentrale Antivirus-Engine Web, FTP, E-Mail und mehr zu scannen. Sie schützt Geräte mit dem Netzwerk verbunden durch Malware zu stoppen, bevor es eine Chance, zu erreichen Ihre Nutzer.';
+$lang['antivirus_app_name'] = 'Gateway-Antivirus';
+$lang['antivirus_app_tooltip'] = 'Verschlüsselte Dateien werden oft verwendet, Malware zu liefern, so wenden Sie sich bitte vorsichtig sein, diese Art von Dateien durch die Antivirus-Engine ermöglicht.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block-verschlüsselten Archiv Wert ist ungültig.';
+$lang['antivirus_block_encrypted_files'] = 'Blockieren von verschlüsselten Dateien';
+$lang['antivirus_every_two_hours'] = 'Alle zwei Stunden';
+$lang['antivirus_maximum_file_size'] = 'Maximale Dateigröße in Zip-Dateien';
+$lang['antivirus_maximum_file_size_invalid'] = 'Die maximale Dateigröße ist ungültig.';
+$lang['antivirus_maximum_files'] = 'Maximale Anzahl Dateien in Zip-Dateien';
+$lang['antivirus_maximum_files_invalid'] = 'Maximale Anzahl der Dateien ist ungültig.';
+$lang['antivirus_maximum_recursion'] = 'Maximale Rekursion in Zip-Dateien';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximale Rekursion ist ungültig.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL Cloaking Scan Zustand ist ungültig.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL-URL Mismatch-Scan-Zustand ist ungültig.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLs Zustand ist ungültig.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing-Signaturen Zustand scannen ist ungültig.';
+$lang['antivirus_twice_a_day'] = 'Zweimal am Tag';
+$lang['antivirus_update_interval'] = 'Updateintervall';
+$lang['antivirus_updates_per_day_invalid'] = 'Anzahl der Updates pro Tag ist ungültig.';
