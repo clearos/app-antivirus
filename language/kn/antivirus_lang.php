6085ded550c8e15cc88de57d1ac253d8e8ca914e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:15:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'ಗೇಟ್ವೇ ಆಂಟಿವೈರಸ್ ಅಪ್ಲಿಕೇಶನ್ ವೆಬ್, FTP ಯ, ಮೇಲ್ ಮತ್ತು ಹೆಚ್ಚು ಸ್ಕ್ಯಾನ್ ಕೇಂದ್ರ ಆಂಟಿವೈರಸ್ ಎಂಜಿನ್ ಅನ್ನು ಬಳಸುತ್ತದೆ. ಇದು ನಿಮ್ಮ ಬಳಕೆದಾರರು ತಲುಪಲು ಅವಕಾಶವಿರುತ್ತದೆ ಮೊದಲು ಮಾಲ್ವೇರ್ ನಿಲ್ಲಿಸುವ ಮೂಲಕ ನಿಮ್ಮ ಜಾಲಕ್ಕೆ ಸಂಪರ್ಕ ಸಾಧನಗಳು ರಕ್ಷಿಸುತ್ತದೆ.';
+$lang['antivirus_app_name'] = 'ಗೇಟ್ವೇ ಆಂಟಿವೈರಸ್';
+$lang['antivirus_app_tooltip'] = 'ಎನ್ಕ್ರಿಪ್ಟ್ ಕಡತಗಳನ್ನು ಸಾಮಾನ್ಯವಾಗಿ ಮಾಲ್ವೇರ್ ತಲುಪಿಸಲು, ಆದ್ದರಿಂದ ಆಂಟಿವೈರಸ್ ಎಂಜಿನ್ ಮೂಲಕ ಕಡತಗಳನ್ನು ಈ ರೀತಿಯ ಅವಕಾಶ ಎಚ್ಚರದಿಂದ ಬಳಸಲಾಗುತ್ತದೆ.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'ಬ್ಲಾಕ್ ಎನ್ಕ್ರಿಪ್ಟ್ ಆರ್ಕೈವ್ ಮೌಲ್ಯ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_block_encrypted_files'] = 'ಬ್ಲಾಕ್ ಎನ್ಕ್ರಿಪ್ಟ್ ಕಡತಗಳನ್ನು';
+$lang['antivirus_every_two_hours'] = 'ಪ್ರತಿ ಎರಡು ಗಂಟೆಗಳಿಗೊಮ್ಮೆ';
+$lang['antivirus_maximum_file_size'] = 'ಜಿಪ್ ಫೈಲ್ಸ್ ಕಡತದ ಗರಿಷ್ಟ ಗಾತ್ರ';
+$lang['antivirus_maximum_file_size_invalid'] = 'ಕಡತದ ಗಾತ್ರದ ಮಿತಿಯನ್ನು ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_maximum_files'] = 'ಜಿಪ್ ಕಡತಗಳನ್ನು ಗರಿಷ್ಠ ಫೈಲ್ಸ್';
+$lang['antivirus_maximum_files_invalid'] = 'ಕಡತಗಳನ್ನು ಗರಿಷ್ಠ ಸಂಖ್ಯೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_maximum_recursion'] = 'ಗರಿಷ್ಠ ಪ್ರತ್ಯಾವರ್ತನೆ ಜಿಪ್ ಫೈಲ್ಸ್';
+$lang['antivirus_maximum_recursion_invalid'] = 'ಗರಿಷ್ಠ ಪುನರಾವರ್ತನ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'ಎಸ್ಎಸ್ಎಲ್ ಮರೆಯಾಗುವುದು ಸ್ಕ್ಯಾನ್ ರಾಜ್ಯದ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'ಎಸ್ಎಸ್ಎಲ್ URL ಅನ್ನು ಅಸಾಮರಸ್ಯವು ಸ್ಕ್ಯಾನ್ ರಾಜ್ಯದ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'ಸ್ಕ್ಯಾನ್ URL ಗಳನ್ನು ರಾಜ್ಯದ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'ಸಹಿಯನ್ನು ರಾಜ್ಯದ ಸ್ಕ್ಯಾನ್ ಫಿಶಿಂಗ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['antivirus_twice_a_day'] = 'ದಿನಕ್ಕೆ ಎರಡು ಬಾರಿ';
+$lang['antivirus_update_interval'] = 'ಅಪ್ಡೇಟ್ ಮಧ್ಯಂತರ';
+$lang['antivirus_updates_per_day_invalid'] = 'ದಿನಕ್ಕೆ ನವೀಕರಣಗಳನ್ನು ಸಂಖ್ಯೆ ಅಮಾನ್ಯವಾಗಿದೆ.';
