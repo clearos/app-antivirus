4799e7419156bc9cd0ad0eb8b9877bfd5d535616
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:18:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Aplicația Gateway Antivirus utilizează un motor antivirus central pentru a scana web, FTP, e-mail și multe altele. Acesta protejează dispozitivele conectate la rețea prin oprirea malware-ului, înainte de a fi o sansa de a ajunge la utilizatori.';
+$lang['antivirus_app_name'] = 'gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Fișierele criptate sunt adesea folosite pentru a livra malware, deci vă rugăm să fiți atent cu privire la care să permită acestor tipuri de fișiere prin intermediul motorului antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloc de valoare arhivă criptată nu este validă.';
+$lang['antivirus_block_encrypted_files'] = 'Bloc fișiere Criptate';
+$lang['antivirus_every_two_hours'] = 'La fiecare două ore';
+$lang['antivirus_maximum_file_size'] = 'Dimensiunea maximă a fișierului în Fișiere Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'Dimensiunea maximă a fișierului este nevalid.';
+$lang['antivirus_maximum_files'] = 'Fișierele maxime în Fișiere Zip';
+$lang['antivirus_maximum_files_invalid'] = 'Numărul maxim de fișiere este nevalid.';
+$lang['antivirus_maximum_recursion'] = 'Maxim Recursia în Fișiere Zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursivitate maximă este nevalid.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL stare scanare camuflare este nevalid.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL-ul de nepotrivire de stat de scanare este nevalid.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scanare URL-uri de stat nu este validă.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Semnături de phishing scanare de stat nu este validă.';
+$lang['antivirus_twice_a_day'] = 'De doua ori pe zi';
+$lang['antivirus_update_interval'] = 'Interval de actualizare';
+$lang['antivirus_updates_per_day_invalid'] = 'Numărul de actualizări pe zi nu este validă.';
