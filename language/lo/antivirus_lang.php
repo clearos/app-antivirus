3cd88cdf29f2cd141738340b7db2a1e68b3e49c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:15:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'ການ app Gateway Antivirus ການນໍາໃຊ້ເຄື່ອງຈັກໃນການປ້ອງກັນໄວລັດສູນກາງການສະແກນເວັບໄຊທ໌ FTP, mail ແລະຫຼາຍ. ມັນປົກປ້ອງອຸປະກອນເຊື່ອມຕໍ່ກັບເຄືອຂ່າຍຂອງທ່ານໂດຍການຢຸດ malware ກ່ອນທີ່ຈະມີໂອກາດທີ່ຈະສາມາດບັນລຸຜູ້ຊົມໃຊ້ຂອງທ່ານ.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'ໄຟລ໌ encrypted ໄດ້ຖືກນໍາໃຊ້ເພື່ອໃຫ້ malware, ສະນັ້ນກະລຸນາຈະລະມັດລະວັງກ່ຽວກັບການອະນຸຍາດໃຫ້ປະເພດເຫຼົ່ານີ້ຂອງໄຟລ໌ໂດຍຜ່ານເຄື່ອງຈັກປ້ອງກັນໄວລັດ.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block ມູນຄ່າເກັບເຂົ້າລະຫັດບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_block_encrypted_files'] = 'Block Files ການເຂົ້າລະຫັດ';
+$lang['antivirus_every_two_hours'] = 'ທຸກໆສອງຊົ່ວໂມງ';
+$lang['antivirus_maximum_file_size'] = 'ຂະຫນາດພາບສູງສຸດໃນໄຟລ໌ຫັດໄປສະນີ';
+$lang['antivirus_maximum_file_size_invalid'] = 'ຂະຫນາດພາບສູງສຸດແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_maximum_files'] = 'ໄຟລ໌ສູງສຸດໃນໄຟລ໌ຫັດໄປສະນີ';
+$lang['antivirus_maximum_files_invalid'] = 'ຈໍານວນສູງສຸດຂອງໄຟລ໌ບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_maximum_recursion'] = 'Recursion ສູງສຸດໃນໄຟລ໌ຫັດໄປສະນີ';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion ສູງສຸດແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL ລັດ scan cloaking ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL ບໍ່ກົງກັນລັດ scan ແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'ລັດ Scan ທີ່ຢູ່ເວັບໄຊແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing ລາຍເຊັນສະແກນລັດແມ່ນບໍ່ຖືກຕ້ອງ.';
+$lang['antivirus_twice_a_day'] = 'ສອງຄັ້ງຕໍ່ມື້';
+$lang['antivirus_update_interval'] = 'ປັບປຸງໃຫ້ທັນໄລຍະຫ່າງ';
+$lang['antivirus_updates_per_day_invalid'] = 'ຈໍານວນຂອງການປັບປຸງຕໍ່ມື້ແມ່ນບໍ່ຖືກຕ້ອງ.';
