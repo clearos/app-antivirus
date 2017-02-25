4284fcce99c417c44e7bfc766e7d4ad0dd096b83
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:13:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'A aplicación de pasarela antivirus usa un motor antivirus central para dixitalizar web, FTP, correo electrónico e moito máis. El protexe os dispositivos conectados á súa rede, parando malware antes que teña a oportunidade de acadar os seus usuarios.';
+$lang['antivirus_app_name'] = 'pasarela de antivirus';
+$lang['antivirus_app_tooltip'] = 'Os arquivos criptografada son moitas veces utilizados para entregar malware, polo que teña coidado sobre permitir que estes tipos de ficheiros a través do mecanismo antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloque valor de ficheiro cifrado non é válido.';
+$lang['antivirus_block_encrypted_files'] = 'Bloquear arquivos criptografada';
+$lang['antivirus_every_two_hours'] = 'Cada dúas horas';
+$lang['antivirus_maximum_file_size'] = 'Tamaño máximo do arquivo en arquivos Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'tamaño máximo do ficheiro non é válido.';
+$lang['antivirus_maximum_files'] = 'Arquivos máximos en arquivos Zip';
+$lang['antivirus_maximum_files_invalid'] = 'Número máximo de ficheiros non é válido.';
+$lang['antivirus_maximum_recursion'] = 'A recursão máxima en arquivos Zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursão máxima non é válido.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL estado varrido cloaking é válido.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL incompatibilidade estado de verificación non é válido.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Estado URL Scania é válido.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing sinaturas dixitalizar estado non é válido.';
+$lang['antivirus_twice_a_day'] = 'Dúas veces ao día';
+$lang['antivirus_update_interval'] = 'Intervalo de actualización';
+$lang['antivirus_updates_per_day_invalid'] = 'Número de actualizacións diarias non é válido.';
