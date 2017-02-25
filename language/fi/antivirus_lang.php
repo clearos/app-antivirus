29e2fd344af48a889b577c7e6febf3635aa3fa10
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:12:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus app käyttää keskeinen antivirus moottori skannata web, FTP, sähköposti ja paljon muuta. Se suojaa laitteita kytketty verkkoon pysäyttämällä haittaohjelmat ennen kuin se on mahdollisuus päästä käyttäjille.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Salatut tiedostot ovat usein toimittaa haittaohjelmia, joten ole varovainen, että kyseisillä tyyppisten tiedostojen kautta antivirus moottori.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Estä salattu arkisto arvo on virheellinen.';
+$lang['antivirus_block_encrypted_files'] = 'Estä salatut tiedostot';
+$lang['antivirus_every_two_hours'] = 'Joka toinen tunti';
+$lang['antivirus_maximum_file_size'] = 'Suurin tiedoston koko Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'Tiedoston enimmäiskoko on virheellinen.';
+$lang['antivirus_maximum_files'] = 'Suurin tiedostot Zip';
+$lang['antivirus_maximum_files_invalid'] = 'Tiedostojen enimmäismäärä on virheellinen.';
+$lang['antivirus_maximum_recursion'] = 'Suurin Recursion in Zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'Suurin rekursio on virheellinen.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL peittämistä scan tila ei kelpaa.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL epäsuhta scan tila ei kelpaa.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL-osoitteiden tila ei kelpaa.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing allekirjoitukset skannata tila ei kelpaa.';
+$lang['antivirus_twice_a_day'] = 'Kahdesti päivässä';
+$lang['antivirus_update_interval'] = 'Update Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Päivitysten määrä päivässä on virheellinen.';
