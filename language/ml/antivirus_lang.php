15e78c922a8f02184788c7cf4a269faa8d8877bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:16:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'ഗേറ്റ്വേ ആന്റിവൈറസ് അപ്ലിക്കേഷൻ വെബ്, എഫ്ടിപി, മെയിൽ കൂടുതൽ സ്കാൻ ചെയ്യാൻ കേന്ദ്ര ആന്റിവൈറസ് എഞ്ചിൻ ഉപയോഗിക്കുന്നു. ഇത് നിങ്ങളുടെ ഉപയോക്താവിന് എത്താൻ അവസരം മുമ്പിൽ മാൽവെയർ കൈയേറ്റങ്ങളും വഴി നിങ്ങളുടെ നെറ്റ്വർക്കിൽ കണക്ട് ഡിവൈസുകൾ സംരക്ഷിക്കുന്നു.';
+$lang['antivirus_app_name'] = 'ഗേറ്റ്വേ ആന്റിവൈറസ്';
+$lang['antivirus_app_tooltip'] = 'എൻക്രിപ്റ്റ് ഫയലുകൾ പലപ്പോഴും മാൽവെയർ വിടുവിപ്പാൻ ഉപയോഗിക്കുന്ന, അതിനാൽ ആന്റിവൈറസ് എഞ്ചിൻ വഴി ഫയലുകൾ ഈ തരം അനുവദിക്കുന്നതിനെക്കുറിച്ച് ദയവായി ശ്രദ്ധയോടെ.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'ബ്ലോക്ക് ആർക്കൈവ് മൂല്യം അസാധുവാണ് എൻക്രിപ്റ്റ്.';
+$lang['antivirus_block_encrypted_files'] = 'ബ്ലോക്ക് എൻക്രിപ്റ്റ് ഫയലുകൾ';
+$lang['antivirus_every_two_hours'] = 'ഓരോ രണ്ട് മണിക്കൂർ';
+$lang['antivirus_maximum_file_size'] = 'പിൻ ഫയലുകളിൽ പരമാവധി ഫയൽ വലുപ്പം';
+$lang['antivirus_maximum_file_size_invalid'] = 'പരമാവധി ഫയൽ വലുപ്പം അസാധുവാണ്.';
+$lang['antivirus_maximum_files'] = 'പിൻ ഫയലുകളിൽ പരമാവധി ഫയലുകൾ';
+$lang['antivirus_maximum_files_invalid'] = 'ഫയലുകൾ പരമാവധി എണ്ണം അസാധുവാണ്.';
+$lang['antivirus_maximum_recursion'] = 'പിൻ ഫയലുകളിൽ പരമാവധി ആവർത്തനം';
+$lang['antivirus_maximum_recursion_invalid'] = 'പരമാവധി വിശകലനത്തിനുവേണ്ടിയാണീ അസാധുവാണ്.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'സ്കാൻ സംസ്ഥാന ക്ലോക്കിംഗ് എസ്എസ്എൽ അസാധുവാണ്.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'എസ്എസ്എൽ യുആർഎൽ പൊരുത്തക്കേട് സ്കാൻ സംസ്ഥാന അസാധുവാണ്.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'സ്കാൻ URL- കൾ സംസ്ഥാന അസാധുവാണ്.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'ഫിഷിംഗ് ഒപ്പുകൾ സംസ്ഥാന അസാധുവാണ് സ്കാൻ.';
+$lang['antivirus_twice_a_day'] = 'ഒരു ദിവസത്തിൽ രണ്ടു തവണ';
+$lang['antivirus_update_interval'] = 'അപ്ഡേറ്റ് ഇടവേള';
+$lang['antivirus_updates_per_day_invalid'] = 'പ്രതിദിനം അപ്ഡേറ്റുകൾ എണ്ണം അസാധുവാണ്.';
