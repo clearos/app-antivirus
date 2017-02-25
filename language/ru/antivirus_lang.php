9335556f71963a7c66e0dadde71cb7c3fab0e6ec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:18:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Приложение Шлюз Antivirus использует центральный антивирус для сканирования Web, FTP, почту и многое другое. Он защищает устройства, подключенные к сети, остановив вредоносных программ, прежде чем у него есть шанс достичь своих пользователей.';
+$lang['antivirus_app_name'] = 'Шлюз Antivirus';
+$lang['antivirus_app_tooltip'] = 'Зашифрованные файлы часто используются для доставки вредоносных программ, поэтому, пожалуйста, будьте осторожны о разрешении этих типов файлов через антивирусного ядра.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блок зашифрованное значение архива является недействительным.';
+$lang['antivirus_block_encrypted_files'] = 'Блок зашифрованных файлов';
+$lang['antivirus_every_two_hours'] = 'Каждые два часа';
+$lang['antivirus_maximum_file_size'] = 'Максимальный размер файла в Zip-файлов';
+$lang['antivirus_maximum_file_size_invalid'] = 'Максимальный размер файла недействителен.';
+$lang['antivirus_maximum_files'] = 'Максимальное количество файлов в Zip-файлов';
+$lang['antivirus_maximum_files_invalid'] = 'Максимальное количество файлов является недействительным.';
+$lang['antivirus_maximum_recursion'] = 'Максимальная Рекурсия в Zip-файлов';
+$lang['antivirus_maximum_recursion_invalid'] = 'Максимальная рекурсия является недействительным.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL маскировка состояние сканирования является недопустимым.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'состояние сканирования SSL URL несоответствие является недействительным.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Сканирование URL-адресов состояние является недопустимым.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Фишинг сигнатуры сканирования состояния является недействительным.';
+$lang['antivirus_twice_a_day'] = 'Два раза в день';
+$lang['antivirus_update_interval'] = 'Интервал обновления';
+$lang['antivirus_updates_per_day_invalid'] = 'Количество обновлений в день является недействительным.';
