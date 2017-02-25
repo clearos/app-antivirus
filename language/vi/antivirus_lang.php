59cb8557346f1b962720cdb34135aa6c6cae8956
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:21:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Các ứng dụng Gateway Antivirus sử dụng một công cụ chống virus trung tâm để quét web, FTP, mail và nhiều hơn nữa. Nó bảo vệ các thiết bị kết nối vào mạng của bạn bằng cách ngăn chặn phần mềm độc hại trước khi nó có cơ hội để tiếp cận người dùng của bạn.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'file mã hóa thường được sử dụng để cung cấp những phần mềm độc hại, vì vậy hãy cẩn thận về việc cho phép các loại tập tin thông qua các công cụ chống virus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Khối giá trị lưu trữ mật mã không hợp lệ.';
+$lang['antivirus_block_encrypted_files'] = 'Khối mã hóa tập tin';
+$lang['antivirus_every_two_hours'] = 'Mỗi Hai giờ';
+$lang['antivirus_maximum_file_size'] = 'Kích thước tối đa trong tập tin Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'Kích thước tối đa là không hợp lệ.';
+$lang['antivirus_maximum_files'] = 'Tập tin tối đa trong tập tin Zip';
+$lang['antivirus_maximum_files_invalid'] = 'Số lượng tối đa của tập tin này là không hợp lệ.';
+$lang['antivirus_maximum_recursion'] = 'Đệ quy tối đa trong tập tin Zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'đệ quy tối đa là không hợp lệ.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL che đậy quét nhà nước là không hợp lệ.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL không phù hợp quét nhà nước là không hợp lệ.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'trạng thái quét URL không hợp lệ.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing chữ ký quét nhà nước là không hợp lệ.';
+$lang['antivirus_twice_a_day'] = 'Hai lần một ngày';
+$lang['antivirus_update_interval'] = 'cập nhật Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Số tin cập nhật mỗi ngày là không hợp lệ.';
