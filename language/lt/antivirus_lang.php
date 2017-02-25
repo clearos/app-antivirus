bdf31f8d934510925fda5b1b64bb790eb4378d64
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:15:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Vartai Antivirusinė programa naudoja centrinę antivirusinę programą nuskaityti interneto, FTP, paštą ir daugiau. Jis apsaugo įrenginius, prijungtus prie tinklo nutraukiant kenkėjai, kol ji turi galimybę pasiekti vartotojus.';
+$lang['antivirus_app_name'] = 'vartai Antivirusinė';
+$lang['antivirus_app_tooltip'] = 'Užšifruotus failus dažnai naudojamas teikti kenkėjai, todėl būkite atsargūs, apie leidžiantis šių tipų failus per antivirusinės variklis.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blokuoti saugiame archyvas vertė yra neteisinga.';
+$lang['antivirus_block_encrypted_files'] = 'Blokuoti užšifruoti failus';
+$lang['antivirus_every_two_hours'] = 'Kas dvi valandas';
+$lang['antivirus_maximum_file_size'] = 'Maksimalus failo dydis Zip failai';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimalus failo dydis yra neteisingas.';
+$lang['antivirus_maximum_files'] = 'Didžiausios failai zip failus';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimalus failų skaičius yra neteisinga.';
+$lang['antivirus_maximum_recursion'] = 'Maksimalus rekursija į zip failus';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimalus rekursija yra neteisinga.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL maskavimas nuskaitymo būsena yra negaliojantis.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL neatitikimas nuskaitymo būsena yra negaliojantis.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Nuskaitymo URL būsena yra negaliojantis.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing parašai nuskaityti būklę yra neteisinga.';
+$lang['antivirus_twice_a_day'] = 'Du kartus per dieną';
+$lang['antivirus_update_interval'] = 'Atnaujinimo intervalas';
+$lang['antivirus_updates_per_day_invalid'] = 'Taškų naujienas per dieną yra neteisinga.';
