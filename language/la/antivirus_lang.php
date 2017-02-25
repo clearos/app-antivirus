ef51416089ada9aee09b1705b52185fae52e2125
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:15:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Praesent Antivirus App Engine et scan in telam ponit medium Antivirus, FTP, et mail. Quod protegit tua fabrica retiaculis coniuncta est, uidelicet, quod est equivalent ad malware in conspectu tuo users.';
+$lang['antivirus_app_name'] = 'Antivirus Gateway';
+$lang['antivirus_app_tooltip'] = 'Saepe Encrypted lima tradere malware, placet diligenter esse circa typus of lima quod haec per Antivirus engine.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block encrypted Video pretium nulla.';
+$lang['antivirus_block_encrypted_files'] = 'Block Encrypted Files';
+$lang['antivirus_every_two_hours'] = 'Omnis duo horas';
+$lang['antivirus_maximum_file_size'] = 'Maximum File Size V in Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maximum file magnitudine est invalidum.';
+$lang['antivirus_maximum_files'] = 'Maximum Files in V Files';
+$lang['antivirus_maximum_files_invalid'] = 'Maximum numerus lima nulla.';
+$lang['antivirus_maximum_recursion'] = 'Maximum Recursion in V Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximum invalida est, modicum.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL uestiende scan statu est invalidum.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'URL SSL mismatch scan statu est invalidum.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLs statu est invalidum.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing subscriptione currit statu, invalidum est.';
+$lang['antivirus_twice_a_day'] = 'Bis in die';
+$lang['antivirus_update_interval'] = 'Intervallum Update';
+$lang['antivirus_updates_per_day_invalid'] = 'Number of updates per diem, invalidum est.';
