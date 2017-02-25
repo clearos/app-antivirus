cb8e236e421266c0d2aa6bab391a770438da5c87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:15:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Шлюз Антивирус Қолданба Вебті, FTP, поштаны және одан сканерлеу үшін орталық антивирус қозғалтқыш пайдаланады. Ол сіздің пайдаланушыларына қол жеткізуге мүмкіндік бар, бұрын зиянды тоқтату арқылы желіге қосылған құрылғыларды қорғайды.';
+$lang['antivirus_app_name'] = 'Шлюз Антивирус';
+$lang['antivirus_app_tooltip'] = 'Шифрланған файлдарды жиі зиянды жеткізу үшін пайдаланылады, сондықтан антивирустық қозғалтқыштың арқылы файлдарды осы түрлерін беретін шамамен абай болыңыз өтінеміз.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блок мұрағаты мән жарамсыз шифрланған.';
+$lang['antivirus_block_encrypted_files'] = 'Блок шифрланған файлдарды';
+$lang['antivirus_every_two_hours'] = 'Әр екі сағат сайын';
+$lang['antivirus_maximum_file_size'] = 'Қысылған файлдарды ең үлкен файл өлшемін';
+$lang['antivirus_maximum_file_size_invalid'] = 'Ең үлкен файл өлшемі жарамсыз болып табылады.';
+$lang['antivirus_maximum_files'] = 'Қысылған файлдарды Ең Файлдар';
+$lang['antivirus_maximum_files_invalid'] = 'файлдар максималды саны жарамсыз болып табылады.';
+$lang['antivirus_maximum_recursion'] = 'Қысылған файлдарды Ең Recursion';
+$lang['antivirus_maximum_recursion_invalid'] = 'Ең Рекурсия жарамсыз болып табылады.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'Сканерлеу мемлекетті маскировке SSL жарамсыз болып табылады.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL сәйкессіздігі сканерлеу мемлекеттік жарамсыз болып табылады.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Сканерлеу URL мекенжайлары мемлекеттік жарамсыз болып табылады.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Фишинг қолтаңбалар мемлекеттік жарамсыз сканерлеу.';
+$lang['antivirus_twice_a_day'] = 'Күніне екі рет';
+$lang['antivirus_update_interval'] = 'жаңарту аралығы';
+$lang['antivirus_updates_per_day_invalid'] = 'тәулігіне жаңартулар саны жарамсыз болып табылады.';
