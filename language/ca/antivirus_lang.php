58d9d0df006444a014db4887e6ecde5854ca71fa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:11:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'L&#39;aplicació de inici Antivirus utilitza un motor antivirus per escanejar el centre de Web, FTP, correu electrònic i més. Protegeix els dispositius connectats a la xarxa per aturar el malware abans que tingui l&#39;oportunitat d&#39;arribar als seus usuaris.';
+$lang['antivirus_app_name'] = 'Porta d&#39;enllaç Antivirus';
+$lang['antivirus_app_tooltip'] = 'Els arxius xifrats sovint s&#39;utilitzen per lliurar el malware, així que si us plau vagi amb compte pel que fa a permetre aquest tipus d&#39;arxius a través del motor antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloc de valors d&#39;arxiu xifrat no és vàlid.';
+$lang['antivirus_block_encrypted_files'] = 'Bloquejar arxius xifrats';
+$lang['antivirus_every_two_hours'] = 'Cada dues hores';
+$lang['antivirus_maximum_file_size'] = 'Mida màxima de l&#39;arxiu en arxius zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'La mida màxima de fitxer no és vàlid.';
+$lang['antivirus_maximum_files'] = 'Nombre màxim d&#39;arxius en els arxius zip';
+$lang['antivirus_maximum_files_invalid'] = 'Nombre màxim de fitxers no és vàlid.';
+$lang['antivirus_maximum_recursion'] = 'Màxima recursivitat en arxius zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursivitat no és vàlid.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL estat d&#39;exploració de camuflatge no és vàlid.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'Estat d&#39;exploració SSL URL desajust no és vàlid.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Estat d&#39;exploració URL no és vàlida.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing signatures consulta de l&#39;estat no és vàlid.';
+$lang['antivirus_twice_a_day'] = 'Dues vegades al dia';
+$lang['antivirus_update_interval'] = 'Interval d&#39;actualització';
+$lang['antivirus_updates_per_day_invalid'] = 'Nombre d&#39;actualitzacions per dia no és vàlid.';
