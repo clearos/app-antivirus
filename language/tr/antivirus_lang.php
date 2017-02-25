8936f91a5d3781c55a459d386745e29e09b37586
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:20:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Ağ Geçidi Antivirüs uygulaması, web, FTP, mail ve daha taramak için merkezi bir antivirüs motoru kullanır. O senin kullanıcılarına ulaşmak için bir şansı var önce kötü amaçlı yazılım durdurarak ağınıza bağlı cihazların korur.';
+$lang['antivirus_app_name'] = 'Ağ geçidi Antivirus';
+$lang['antivirus_app_tooltip'] = 'Şifreli dosyalar genellikle kötü amaçlı yazılım teslim, bu yüzden antivirüs motoru sayesinde bu tür dosyaları izin konusunda dikkatli olun kullanılır.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok şifreli arşiv değeri geçersiz.';
+$lang['antivirus_block_encrypted_files'] = 'Blok Şifreli Dosyalar';
+$lang['antivirus_every_two_hours'] = 'Her iki Saat';
+$lang['antivirus_maximum_file_size'] = 'Zip Files Maksimum Dosya Boyutu';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimum dosya boyutu geçersiz.';
+$lang['antivirus_maximum_files'] = 'Zip Files Maksimum Dosya';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimum dosya sayısı geçersiz.';
+$lang['antivirus_maximum_recursion'] = 'Zip Files maksimum Özyineleme';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimum yineleme geçersiz.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL gizleme tarama durumu geçersiz.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL uyuşmazlığı tarama durumu geçersiz.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Tarama URL&#39;ler durumu geçersiz.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'imzalar devlet taraması Phishing geçersiz.';
+$lang['antivirus_twice_a_day'] = 'Günde iki kere';
+$lang['antivirus_update_interval'] = 'Güncelleme aralığı';
+$lang['antivirus_updates_per_day_invalid'] = 'Günde güncellemeleri numarası geçersiz.';
