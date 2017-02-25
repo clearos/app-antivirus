17e0001f344c45344eaf95f5e413db817c754254
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:20:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Дар барнома Gateway Антивирусҳо мебарад муҳаррики Антивирус марказии ба скан веб, FTP, почтаи электронӣ ва бештар. Ин дастгоҳҳо бо қатъ зараровар пеш аз он дорад, имконият барои расидан ба истифодабарандагони худро ба шабакаи худ пайваст, муҳофизат мекунад.';
+$lang['antivirus_app_name'] = 'дарвоза Антивирусҳо';
+$lang['antivirus_app_tooltip'] = 'файлҳои рамзкунонидашуда аксаран истифода бурда расонидани зараровар, то лутфан эҳтиёт имкон ин навъи файлҳои ба воситаи муҳаррики Антивирус шавад.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блоки арзиши бойгонии рамзкунонидашуда нодуруст аст.';
+$lang['antivirus_block_encrypted_files'] = 'Блоки рамзкунонидашуда Files';
+$lang['antivirus_every_two_hours'] = 'Ҳар ду соат';
+$lang['antivirus_maximum_file_size'] = 'Андозаи файли максималӣ дар Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Андозаи файли максималиро нодуруст аст.';
+$lang['antivirus_maximum_files'] = 'Файлҳои Максимум дар Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Баландтарин миқдори файлҳои нодуруст аст.';
+$lang['antivirus_maximum_recursion'] = 'Максимум Recursion дар Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Максимум recursion нодуруст аст.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL давлатии скан cloaking нодуруст аст.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL мутобиқат намекунад давлатӣ скан нодуруст аст.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'давлат шарту URL-нодуруст аст.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Фишинг имзоҳо скан давлатӣ нодуруст аст.';
+$lang['antivirus_twice_a_day'] = 'Ду маротиба дар як рӯз,';
+$lang['antivirus_update_interval'] = 'навсозии фосилавӣ';
+$lang['antivirus_updates_per_day_invalid'] = 'Миқдори навсозиҳои дар як рўз нодуруст аст.';
