4ea94964d3b9d1334df9ccc98ccb8d9135a8ee90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:20:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'గేట్వే యాంటీవైరస్ అనువర్తనం వెబ్, FTP, మెయిల్ మరియు మరిన్ని స్కాన్ ఒక కేంద్ర యాంటీవైరస్ ఇంజిన్ను ఉపయోగిస్తుంది. ఇది మీ వినియోగదారులను చేరుకోవడానికి అవకాశం ఉంటుంది ముందు మాల్వేర్ ఆపివేయడం ద్వారా మీ నెట్వర్క్కు కనెక్ట్ పరికరాలు రక్షిస్తుంది.';
+$lang['antivirus_app_name'] = 'గేట్వే యాంటీవైరస్';
+$lang['antivirus_app_tooltip'] = 'ఎన్క్రిప్టెడ్ ఫైళ్లు తరచుగా మాల్వేర్ బట్వాడా, కాబట్టి యాంటీవైరస్ ఇంజిన్ ద్వారా ఫైళ్ళను ఈ రకమైన అనుమతించడం గురించి జాగ్రత్తగా ఉండండి ఉపయోగిస్తారు.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'బ్లాక్ ఎన్క్రిప్టెడ్ ఆర్కైవ్ విలువ చెల్లదు.';
+$lang['antivirus_block_encrypted_files'] = 'బ్లాక్ ఎన్క్రిప్టెడ్ ఫైళ్ళు';
+$lang['antivirus_every_two_hours'] = 'ప్రతి రెండు గంటల';
+$lang['antivirus_maximum_file_size'] = 'జిప్ ఫైళ్ళు గరిష్ఠ ఫైలు సైజు';
+$lang['antivirus_maximum_file_size_invalid'] = 'గరిష్ట ఫైలు పరిమాణం చెల్లనిది.';
+$lang['antivirus_maximum_files'] = 'జిప్ ఫైళ్ళు గరిష్ఠ ఫైళ్ళు';
+$lang['antivirus_maximum_files_invalid'] = 'ఫైళ్లు గరిష్ట సంఖ్య చెల్లదు.';
+$lang['antivirus_maximum_recursion'] = 'జిప్ ఫైళ్ళు గరిష్ఠ సూత్రం';
+$lang['antivirus_maximum_recursion_invalid'] = 'గరిష్ఠ సూత్రం చెల్లదు.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL మోసపూరితం స్కాన్ రాష్ట్ర చెల్లదు.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL అసమతుల్యత స్కాన్ రాష్ట్ర చెల్లదు.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'స్కాన్ URL లు రాష్ట్ర చెల్లదు.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'చౌర్య సంతకాలు రాష్ట్ర స్కాన్ చెల్లదు.';
+$lang['antivirus_twice_a_day'] = 'రోజుకు రెండు సార్లు';
+$lang['antivirus_update_interval'] = 'నవీకరణ విరామం';
+$lang['antivirus_updates_per_day_invalid'] = 'రోజుకు నవీకరణలను సంఖ్య చెల్లనిది.';
