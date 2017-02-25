ca3072e3e33e7b64b27cc83c348285a15cc73aac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:12:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app bruger en central antivirus motor til at scanne web, FTP, mail og mere. Den beskytter enheder tilsluttet til dit netværk ved at stoppe malware før det har en chance for at nå dine brugere.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Krypterede filer bruges ofte til at levere malware, så vær forsigtig med at lade disse typer af filer via antivirusmaskinen.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloker krypteret arkiv værdi er ugyldig.';
+$lang['antivirus_block_encrypted_files'] = 'Block krypterede filer';
+$lang['antivirus_every_two_hours'] = 'Hver to timer';
+$lang['antivirus_maximum_file_size'] = 'Maksimal filstørrelse i Zip-filer';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimal filstørrelse er ugyldig.';
+$lang['antivirus_maximum_files'] = 'Maksimum filer i Zip-filer';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimalt antal filer er ugyldig.';
+$lang['antivirus_maximum_recursion'] = 'Maksimal Rekursion i Zip-filer';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimal rekursion er ugyldig.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan tilstand er ugyldig.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL misforhold scan tilstand er ugyldig.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL&#39;er tilstand er ugyldig.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing signaturer scanne tilstand er ugyldig.';
+$lang['antivirus_twice_a_day'] = 'To gange om dagen';
+$lang['antivirus_update_interval'] = 'opdater Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Antal opdateringer om dagen er ugyldig.';
