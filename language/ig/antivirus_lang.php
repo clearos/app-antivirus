ce672694e344dc81c164c8f41e0d9fd341709c43
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:14:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus na-eji ngwa na-eji a Central antivirus engine ka iṅomi web, FTP, mail na ndị ọzọ. Ọ na-echebe ngwaọrụ jikọọ na netwọk gị site nkwụsị malware tupu ya nwere ohere ruo gị ọrụ.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Ezoro ezo faịlụ na-eji napụta malware, mere nwere ike ịkpachara anya banyere ikwe ndị a na ụdị faịlụ site na antivirus engine.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Ngọngọ ezoro ezo Archive uru bụ ghara ịdị irè.';
+$lang['antivirus_block_encrypted_files'] = 'Ngọngọ ezoro ezo Files';
+$lang['antivirus_every_two_hours'] = 'Awa abụọ ọ bụla';
+$lang['antivirus_maximum_file_size'] = 'Oke File Size na Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Oke file size bụ ghara ịdị irè.';
+$lang['antivirus_maximum_files'] = 'Oke Files na Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Karịa ọnụ ọgụgụ nke faịlụ bụ ghara ịdị irè.';
+$lang['antivirus_maximum_recursion'] = 'Oke Recursion na Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Oke recursion bụ ghara ịdị irè.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking Doppler ala bụ ghara ịdị irè.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch Doppler ala bụ ghara ịdị irè.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Doppler URLs ala bụ ghara ịdị irè.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing signatures ike iṅomi ala bụ ghara ịdị irè.';
+$lang['antivirus_twice_a_day'] = 'Ugboro abụọ a Day';
+$lang['antivirus_update_interval'] = 'update nkeji';
+$lang['antivirus_updates_per_day_invalid'] = 'Number nke mmelite kwa ụbọchị bụ ghara ịdị irè.';
