54257201ba3cd0fb8fd61c31a11312f19d1b8407
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:20:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'แอปพลิเคเกตเวย์ป้องกันไวรัสใช้เครื่องมือป้องกันไวรัสกลางที่จะสแกนเว็บ, FTP, อีเมลและอื่น ๆ จะช่วยปกป้องอุปกรณ์ที่เชื่อมต่อกับเครือข่ายของคุณโดยการหยุดมัลแวร์ก่อนที่จะมีโอกาสที่จะเข้าถึงผู้ใช้ของคุณ';
+$lang['antivirus_app_name'] = 'เกตเวย์ป้องกันไวรัส';
+$lang['antivirus_app_tooltip'] = 'ไฟล์ที่เข้ารหัสมักจะถูกใช้ในการส่งมัลแวร์ดังนั้นโปรดระมัดระวังเกี่ยวกับการอนุญาตเหล่านี้ประเภทของไฟล์ผ่านเครื่องมือป้องกันไวรัส';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'บล็อกเก็บค่าการเข้ารหัสไม่ถูกต้อง';
+$lang['antivirus_block_encrypted_files'] = 'บล็อกไฟล์ที่เข้ารหัส';
+$lang['antivirus_every_two_hours'] = 'ทุกสองชั่วโมง';
+$lang['antivirus_maximum_file_size'] = 'ขนาดไฟล์สูงสุดในไฟล์ ZIP';
+$lang['antivirus_maximum_file_size_invalid'] = 'ขนาดไฟล์สูงสุดไม่ถูกต้อง';
+$lang['antivirus_maximum_files'] = 'ไฟล์สูงสุดในไฟล์ ZIP';
+$lang['antivirus_maximum_files_invalid'] = 'จำนวนสูงสุดของไฟล์ไม่ถูกต้อง';
+$lang['antivirus_maximum_recursion'] = 'Recursion สูงสุดในไฟล์ ZIP';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion สูงสุดไม่ถูกต้อง';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL รัฐสแกนปิดบังไม่ถูกต้อง';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL ที่ไม่ตรงกันรัฐสแกนไม่ถูกต้อง';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'รัฐสแกน URL ที่ไม่ถูกต้อง';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'ฟิชชิ่งลายเซ็นสแกนรัฐไม่ถูกต้อง';
+$lang['antivirus_twice_a_day'] = 'วันละสองครั้ง';
+$lang['antivirus_update_interval'] = 'ปรับปรุงช่วงเวลา';
+$lang['antivirus_updates_per_day_invalid'] = 'จำนวนการปรับปรุงต่อวันไม่ถูกต้อง';
