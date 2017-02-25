bc5bb235b6dd50e1698a06abdbf4bac82062467e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:16:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus програм вэб, FTP, шуудан, илүү скан төв вирусны эсрэг хөдөлгүүрийг ашигладаг. Энэ нь таны хэрэглэгчдэд хүрэх боломж байна өмнө нь үзүүлж чадахгүй зогсоож таны сүлжээнд холбогдсон төхөөрөмжийг хамгаалдаг.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Шифрлэгдсэн файлууд нь байнга үзүүлж чадахгүй хүргэх тул вирусны эсрэг хөдөлгүүр дамжуулан эдгээр төрлийн файлуудыг зөвшөөрөх тухай болгоомжтой байх уу ашигладаг.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блок шифрлэгдсэн архив үнэ цэнэ нь хүчин төгөлдөр бус байна.';
+$lang['antivirus_block_encrypted_files'] = 'Блок Шифрлэгдсэн файлууд';
+$lang['antivirus_every_two_hours'] = 'Бүр хоёр цаг';
+$lang['antivirus_maximum_file_size'] = 'Zip файл дахь хамгийн их файлын хэмжээ';
+$lang['antivirus_maximum_file_size_invalid'] = 'Хамгийн их файлын хэмжээ хүчин төгөлдөр бус байна.';
+$lang['antivirus_maximum_files'] = 'Zip файл дахь хамгийн их файлууд';
+$lang['antivirus_maximum_files_invalid'] = 'файлуудын хамгийн их тоо хүчин төгөлдөр бус байна.';
+$lang['antivirus_maximum_recursion'] = 'Zip файл дахь хамгийн их рекурсын';
+$lang['antivirus_maximum_recursion_invalid'] = 'Хамгийн их рекурсын хүчин төгөлдөр бус байна.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking скан төрийн хүчин төгөлдөр бус байна.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL тохирохгүй скан төрийн хүчин төгөлдөр бус байна.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL-ууд төрийн хүчин төгөлдөр бус байна.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'гарын үсэг төр хайх фишинг хүчин төгөлдөр бус байна.';
+$lang['antivirus_twice_a_day'] = 'Өдөрт хоёр';
+$lang['antivirus_update_interval'] = 'шинэчлэх Интервал';
+$lang['antivirus_updates_per_day_invalid'] = 'Өдөрт шинэчлэлтийн тоо хүчин төгөлдөр бус байна.';
