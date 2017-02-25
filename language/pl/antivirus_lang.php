cfca1cfdc72222d065855c6833796e52c1848373
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:18:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Aplikacja Brama antywirusowa wykorzystuje silnik antywirusowy centralny skanować WWW, FTP, poczta i inne. Chroni urządzenie podłączone do sieci poprzez zatrzymanie malware zanim ma szansę na dotarcie do użytkowników.';
+$lang['antivirus_app_name'] = 'brama antywirusowa';
+$lang['antivirus_app_tooltip'] = 'Zaszyfrowane pliki są często wykorzystywane w celu dostarczenia złośliwego oprogramowania, więc proszę być ostrożnym, dzięki czemu te typy plików za pośrednictwem silnika antywirusowego.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok zaszyfrowana wartość archiwum jest nieprawidłowy.';
+$lang['antivirus_block_encrypted_files'] = 'Blok Encrypted Files';
+$lang['antivirus_every_two_hours'] = 'Co dwie godziny';
+$lang['antivirus_maximum_file_size'] = 'Maksymalny rozmiar pliku w pliki zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksymalny rozmiar pliku jest nieprawidłowy.';
+$lang['antivirus_maximum_files'] = 'Maksymalna ilość plików w zip';
+$lang['antivirus_maximum_files_invalid'] = 'Maksymalna liczba plików jest nieprawidłowy.';
+$lang['antivirus_maximum_recursion'] = 'Maksymalna Rekurencja w pliki zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksymalna rekurencji jest nieprawidłowy.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL maskowanie Stan skanowania jest nieprawidłowy.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL niezgodność stanu skanowania jest nieprawidłowy.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Stan skanowania adresów URL jest nieprawidłowy.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing podpisy skanować stan jest nieprawidłowy.';
+$lang['antivirus_twice_a_day'] = 'Dwa razy dziennie';
+$lang['antivirus_update_interval'] = 'aktualizacja Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Liczba aktualizacji dziennie jest nieprawidłowy.';
