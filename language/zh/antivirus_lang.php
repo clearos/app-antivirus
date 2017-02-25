c771d64f314781620dc23304af3b5217cce16054
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:11:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = '网关防病毒应用程序使用一个中央防病毒引擎来扫描Web，FTP，邮件等等。 它保护通过阻止恶意软件它有一个机会，以达到您的用户之前连接到您的网络设备。';
+$lang['antivirus_app_name'] = '防病毒网关';
+$lang['antivirus_app_tooltip'] = '加密文件经常被用来传播恶意软件，所以请小心让这些类型的文件通过杀毒引擎。';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = '块加密的档案值无效。';
+$lang['antivirus_block_encrypted_files'] = '块加密文件';
+$lang['antivirus_every_two_hours'] = '每两个小时';
+$lang['antivirus_maximum_file_size'] = '在Zip文件最大文件大小';
+$lang['antivirus_maximum_file_size_invalid'] = '最大文件大小是无效的。';
+$lang['antivirus_maximum_files'] = '在Zip文件的最大文件';
+$lang['antivirus_maximum_files_invalid'] = '的最大文件数是无效的。';
+$lang['antivirus_maximum_recursion'] = '最大递归的Zip文件';
+$lang['antivirus_maximum_recursion_invalid'] = '最大递归无效。';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL扫描伪装状态无效。';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL不匹配的扫描状态无效。';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = '扫描的URL状态无效。';
+$lang['antivirus_phishing_signatures_state_invalid'] = '网络钓鱼特征码扫描状态无效。';
+$lang['antivirus_twice_a_day'] = '每天两次';
+$lang['antivirus_update_interval'] = '更新间隔';
+$lang['antivirus_updates_per_day_invalid'] = '每天更新数是无效的。';
