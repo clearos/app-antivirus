b667912d4ae5bfdb9d0c4c793dcabaab0efbab9e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:10:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Дадатак Шлюз Antivirus выкарыстоўвае цэнтральны антывірус для сканавання Web, FTP, пошту і многае іншае. Ён абараняе прылады, падлучаныя да сеткі, спыніўшы шкоднасных праграм, перш чым у яго ёсць шанец дасягнуць сваіх карыстальнікаў.';
+$lang['antivirus_app_name'] = 'шлюз Antivirus';
+$lang['antivirus_app_tooltip'] = 'Зашыфраваныя файлы часта выкарыстоўваюцца для дастаўкі шкоднасных праграм, таму, калі ласка, будзьце асцярожныя аб дазволе гэтых тыпаў файлаў праз антывіруснага ядра.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блок зашыфраванае значэнне архіва з&#39;яўляецца несапраўдным.';
+$lang['antivirus_block_encrypted_files'] = 'Блок зашыфраваных файлаў';
+$lang['antivirus_every_two_hours'] = 'Кожныя дзве гадзіны';
+$lang['antivirus_maximum_file_size'] = 'Максімальны памер файла ў Zip-файлаў';
+$lang['antivirus_maximum_file_size_invalid'] = 'Максімальны памер файла ня дзейнічае.';
+$lang['antivirus_maximum_files'] = 'Максімальную колькасць файлаў у Zip-файлаў';
+$lang['antivirus_maximum_files_invalid'] = 'Максімальную колькасць файлаў з&#39;яўляецца несапраўдным.';
+$lang['antivirus_maximum_recursion'] = 'Максімальная Рэкурсія ў Zip-файлаў';
+$lang['antivirus_maximum_recursion_invalid'] = 'Максімальная Рэкурсія з&#39;яўляецца несапраўдным.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL маскіроўка стан сканавання з&#39;яўляецца недапушчальным.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'стан сканавання SSL URL неадпаведнасць з&#39;яўляецца несапраўдным.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Сканаванне URL-адрасоў стан з&#39;яўляецца недапушчальным.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Фішынг сігнатуры сканавання стану з&#39;яўляецца несапраўдным.';
+$lang['antivirus_twice_a_day'] = 'Два разы на дзень';
+$lang['antivirus_update_interval'] = 'інтэрвал абнаўлення';
+$lang['antivirus_updates_per_day_invalid'] = 'Колькасць абнаўленняў у дзень з&#39;яўляецца несапраўдным.';
