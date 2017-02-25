d8e6b286000d4e1969059ad42a72241e0f4324e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:19:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The app Gateway-adda isticmaalaa engine a antivirus dhexe in iskaan web, FTP, mail oo dheeraad ah. Wuxuu ka hortagaa qalabka ku xiran aad network by joojinta furin ka hor waxa ay leedahay fursad ay ku gaaraan dadka isticmaala aad.';
+$lang['antivirus_app_name'] = 'Gateway Adda';
+$lang['antivirus_app_tooltip'] = 'files Windows XP waxaa inta badan loo isticmaalaa in lagu samatabbixin furin, sidaas darteed fadlan taxaddar ku saabsan jidaynayey noocaan ah files dhex engine antivirus ah.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block qiimaha archive lo waa baadil.';
+$lang['antivirus_block_encrypted_files'] = 'Block lo Files';
+$lang['antivirus_every_two_hours'] = 'Kasta Labo Hours';
+$lang['antivirus_maximum_file_size'] = 'Size File ugu badan ee Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'size file ugu waa baadil.';
+$lang['antivirus_maximum_files'] = 'Files ugu badan ee Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'tirada ugu badan ee files waa baadil.';
+$lang['antivirus_maximum_recursion'] = 'Ugu Recursion in Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion ugu waa baadil.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL gobolka scan cloaking waa baadil.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL jaanqaadayn URL gobolka scan waa baadil.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'gobolka Scan URLs waa baadil.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing saxiixyada iskaan gobolka waa baadil.';
+$lang['antivirus_twice_a_day'] = 'Maalinta Labo a';
+$lang['antivirus_update_interval'] = 'Update bareeg';
+$lang['antivirus_updates_per_day_invalid'] = 'Number of updates maalin kasta waa baadil.';
