a78f0b5a49af8e7e06c92993e5579688e0b61362
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:18:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'برنامه آنتی ویروس دروازه با استفاده از یک موتور آنتی ویروس مرکزی برای اسکن وب، FTP، ایمیل و بیشتر. این دستگاه با متوقف کردن نرم افزارهای مخرب قبل از آن یک فرصت برای رسیدن به کاربران خود را به شبکه شما متصل محافظت می کند.';
+$lang['antivirus_app_name'] = 'دروازه آنتی ویروس';
+$lang['antivirus_app_tooltip'] = 'فایل های رمزگذاری شده اغلب استفاده می شود برای ارائه نرم افزارهای مخرب، پس لطفا مراقب باشید در مورد اجازه دادن به این نوع از فایل ها از طریق موتور آنتی ویروس می باشد.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'بلوک ارزش آرشیو های رمزگذاری شده نامعتبر است.';
+$lang['antivirus_block_encrypted_files'] = 'بلوک رمزگذاری فایل های';
+$lang['antivirus_every_two_hours'] = 'هر دو ساعت';
+$lang['antivirus_maximum_file_size'] = 'حداکثر اندازه فایل در فایل های فشرده';
+$lang['antivirus_maximum_file_size_invalid'] = 'حداکثر اندازه فایل نامعتبر است.';
+$lang['antivirus_maximum_files'] = 'فایل حداکثر در فایل های فشرده';
+$lang['antivirus_maximum_files_invalid'] = 'حداکثر تعداد فایل نامعتبر است.';
+$lang['antivirus_maximum_recursion'] = 'حداکثر بازگشت در فایل های فشرده';
+$lang['antivirus_maximum_recursion_invalid'] = 'حداکثر بازگشت نامعتبر است.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL دولت اسکن پنهان نامعتبر است.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL عدم تطابق دولت اسکن نامعتبر است.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'دولت اسکن URL های نامعتبر است.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'فیشینگ امضا اسکن دولت نامعتبر است.';
+$lang['antivirus_twice_a_day'] = 'دوبار در روز';
+$lang['antivirus_update_interval'] = 'بروزرسانی فاصله';
+$lang['antivirus_updates_per_day_invalid'] = 'تعداد به روز رسانی در هر روز نامعتبر است.';
