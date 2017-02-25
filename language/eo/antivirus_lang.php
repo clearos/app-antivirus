dc397d13352bff1ad08bee87fcc6adf6a9323826
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:12:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'La Enirejo Antivirus app uzas centran antivirus motoro skani retejo, FTP, retpoŝto kaj pli. Ĝi protektas mekanismoj konektitaj al via reto per ĉeso malware antaŭ ĝi havas eblecon atingi viajn uzantojn.';
+$lang['antivirus_app_name'] = 'enirejo Antivirus';
+$lang['antivirus_app_tooltip'] = 'Ĉifrita dosieroj ofte uzita liveri malware, do bonvolu atenti pri permesante tiujn tipojn de dosieroj tra la antivirus motoro.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloko ĉifrita arkivo valoro estas nevalida.';
+$lang['antivirus_block_encrypted_files'] = 'Bloko Ĉifrita Dosieroj';
+$lang['antivirus_every_two_hours'] = 'Ĉiun Du Horoj';
+$lang['antivirus_maximum_file_size'] = 'Maksimuma dosiergrandeco en Poŝtkodo Dosieroj';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimuma grandeco de dosiero estas nevalida.';
+$lang['antivirus_maximum_files'] = 'Maksimuma Dosieroj en Poŝtkodo Dosieroj';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimuma nombro de dosieroj estas nevalida.';
+$lang['antivirus_maximum_recursion'] = 'Maksimuma Rikuro en Poŝtkodo Dosieroj';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimuma rekursio estas nevalida.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scintigrafio stato estas nevalida.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL malkongrua scintigrafio stato estas nevalida.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLoj stato estas nevalida.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing subskribojn skani stato estas nevalida.';
+$lang['antivirus_twice_a_day'] = 'Dufoje tage';
+$lang['antivirus_update_interval'] = 'update Intervalo';
+$lang['antivirus_updates_per_day_invalid'] = 'Numeron de ĝisdatigoj tage estas nevalida.';
