2d95996656cddd54878babea93ff25d8189c3341
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:16:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'गेटवे अँटीव्हायरस अॅप वेब, FTP, मेल आणि अधिक स्कॅन एक मध्यवर्ती अँटीव्हायरस इंजिन वापरते. तो आपल्या वापरकर्त्यांना पोहोचण्याचा एक संधी आहे आधी मालवेअर बंद करून आपल्या नेटवर्क कनेक्ट केलेले साधने संरक्षण होते.';
+$lang['antivirus_app_name'] = 'गेटवे अँटीव्हायरस';
+$lang['antivirus_app_tooltip'] = 'कूटबद्ध फाइल अनेकदा मालवेअर वितरीत, म्हणून कृपया अँटीव्हायरस इंजिन माध्यमातून या प्रकारची फाइल परवानगी काळजी वापरले जातात.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'एनक्रिप्टेड संग्रहण मूल्य ब्लॉक अवैध आहे.';
+$lang['antivirus_block_encrypted_files'] = 'ब्लॉक कूटबद्ध फायली';
+$lang['antivirus_every_two_hours'] = 'दर दोन तासांनी';
+$lang['antivirus_maximum_file_size'] = 'पिन फायली मध्ये जास्तीत जास्त फाइल आकार';
+$lang['antivirus_maximum_file_size_invalid'] = 'कमाल फाइल आकार अवैध आहे.';
+$lang['antivirus_maximum_files'] = 'पिन फायली मध्ये जास्तीत जास्त फायली';
+$lang['antivirus_maximum_files_invalid'] = 'फाइल जास्तीत जास्त अवैध आहे.';
+$lang['antivirus_maximum_recursion'] = 'पिन फायली मध्ये जास्तीत जास्त रीकर्शन';
+$lang['antivirus_maximum_recursion_invalid'] = 'जास्तीत जास्त पुनरावृत्तीच्या अवैध आहे.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL झाकणे स्कॅन राज्य अवैध आहे.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL जुळत नाही स्कॅन राज्य अवैध आहे.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'स्कॅन केलेल्या URL राज्य अवैध आहे.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'स्वाक्षर्या राज्य स्कॅन फिशिंग अवैध आहे.';
+$lang['antivirus_twice_a_day'] = 'दिवसातून दोनदा';
+$lang['antivirus_update_interval'] = 'मध्यांतर सुधारणा';
+$lang['antivirus_updates_per_day_invalid'] = 'दररोज अद्ययावत संख्या अवैध आहे.';
