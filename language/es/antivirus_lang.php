bde59ce1542153411cd49705df79ed442e66812b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:19:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'La aplicación de Gateway Antivirus utiliza un motor antivirus para escanear el centro de Web, FTP, correo electrónico y más. Protege los dispositivos conectados a la red por detener el malware antes de que tenga la oportunidad de llegar a sus usuarios.';
+$lang['antivirus_app_name'] = 'Puerta de enlace Antivirus';
+$lang['antivirus_app_tooltip'] = 'Los archivos cifrados a menudo se utilizan para entregar el malware, así que por favor tenga cuidado en cuanto a permitir este tipo de archivos a través del motor antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloque de valores de archivo cifrado no es válido.';
+$lang['antivirus_block_encrypted_files'] = 'Bloquear archivos cifrados';
+$lang['antivirus_every_two_hours'] = 'Cada dos horas';
+$lang['antivirus_maximum_file_size'] = 'Tamaño máximo del archivo en archivos zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'El tamaño máximo de archivo no es válido.';
+$lang['antivirus_maximum_files'] = 'Número máximo de archivos en los archivos zip';
+$lang['antivirus_maximum_files_invalid'] = 'Número máximo de archivos no es válido.';
+$lang['antivirus_maximum_recursion'] = 'Máxima recursividad en archivos zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursividad no es válido.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL estado de exploración de camuflaje no es válido.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'Estado de exploración SSL URL desajuste no es válido.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Estado de exploración URL no es válida.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing firmas consulta del estado no es válido.';
+$lang['antivirus_twice_a_day'] = 'Dos veces al día';
+$lang['antivirus_update_interval'] = 'Intervalo de actualización';
+$lang['antivirus_updates_per_day_invalid'] = 'Número de actualizaciones por día no es válido.';
