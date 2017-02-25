1a675dc8ddc46cd6783c13ffe045316551b0e294
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:21:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Додаток Шлюз Antivirus використовує центральний антивірус для сканування Web, FTP, пошту і багато іншого. Він захищає пристрої, підключені до мережі, зупинивши шкідливих програм, перш ніж у нього є шанс досягти своїх користувачів.';
+$lang['antivirus_app_name'] = 'Шлюз Antivirus';
+$lang['antivirus_app_tooltip'] = 'Зашифровані файли часто використовуються для доставки шкідливих програм, тому, будь ласка, будьте обережні про дозвіл цих типів файлів через антивірусного ядра.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блок зашифроване значення архіву є недійсним.';
+$lang['antivirus_block_encrypted_files'] = 'Блок зашифрованих файлів';
+$lang['antivirus_every_two_hours'] = 'Кожні дві години';
+$lang['antivirus_maximum_file_size'] = 'Максимальний розмір файлу в Zip-файлів';
+$lang['antivirus_maximum_file_size_invalid'] = 'Максимальний розмір файлу недійсний.';
+$lang['antivirus_maximum_files'] = 'Максимальна кількість файлів в Zip-файлів';
+$lang['antivirus_maximum_files_invalid'] = 'Максимальна кількість файлів є недійсним.';
+$lang['antivirus_maximum_recursion'] = 'Максимальна Рекурсія в Zip-файлів';
+$lang['antivirus_maximum_recursion_invalid'] = 'Максимальна рекурсія є недійсним.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL маскування стан сканування є неприпустимим.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'стан сканування SSL URL невідповідність є недійсним.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Сканування URL-адрес стан є неприпустимим.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Фішинг сигнатури сканування стану є недійсним.';
+$lang['antivirus_twice_a_day'] = 'Два рази на день';
+$lang['antivirus_update_interval'] = 'Частота оновлення';
+$lang['antivirus_updates_per_day_invalid'] = 'Кількість оновлень в день є недійсним.';
