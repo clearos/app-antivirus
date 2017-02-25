371025b2ae0abdaeb85bcc4e59b18581e4a15856
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:14:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app notar miðlæga antivirus vél til að skanna vefinn, FTP, tölvupóst og fleira. Það ver tæki tengd við netið með því að stöðva malware áður en það hefur tækifæri til að ná notendum.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Dulkóðaðar skrár eru oft notuð til að bera malware, svo vinsamlegast vera varkár um að leyfa þessar tegundir af skrám í gegnum antivirus vél.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block dulkóðað skjalasafn gildi er ógilt.';
+$lang['antivirus_block_encrypted_files'] = 'Block dulkóðuð skrá';
+$lang['antivirus_every_two_hours'] = 'Á tveggja klst';
+$lang['antivirus_maximum_file_size'] = 'Hámaks skráarstærð í Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Hámarks skráarstærð er ógild.';
+$lang['antivirus_maximum_files'] = 'Hámark Skrár í Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Hámarks fjöldi skráa er ógilt.';
+$lang['antivirus_maximum_recursion'] = 'Hámarks Recursion í Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Hámarks endurkvæmni er ógilt.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL skikkja skanna ástand er ógilt.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL misræmi skanna ástand er ógilt.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan vefslóðir ástand er ógilt.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing undirskriftir skanna ástand er ógilt.';
+$lang['antivirus_twice_a_day'] = 'Tvisvar á dag';
+$lang['antivirus_update_interval'] = 'Uppfæra Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Fjöldi uppfærslur á dag er ógilt.';
