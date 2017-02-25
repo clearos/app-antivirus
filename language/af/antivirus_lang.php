fd14723077c9c59b2c1ff6ef684afdcf752d1fa4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:10:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Die Gateway Antivirus program &#39;n sentrale antivirus enjin web, FTP, pos en nog baie meer te scan. Dit beskerm toestelle gekoppel aan jou netwerk deur te stop malware voordat dit &#39;n kans om jou gebruikers te bereik.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Geïnkripteer lêers word dikwels gebruik om malware te lewer, so wees asseblief versigtig sodat hierdie soort lêers deur die antivirus-enjin.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok geïnkripteer argief waarde is ongeldig.';
+$lang['antivirus_block_encrypted_files'] = 'Blok Encrypted lêers';
+$lang['antivirus_every_two_hours'] = 'Elke twee ure';
+$lang['antivirus_maximum_file_size'] = 'Maksimum Lêer Grootte in zip-lêers';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimum lêer grootte is ongeldig.';
+$lang['antivirus_maximum_files'] = 'Maksimum lêers in zip-lêers';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimum aantal lêers is ongeldig.';
+$lang['antivirus_maximum_recursion'] = 'Maksimum Rekursie in zip-lêers';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimum rekursie is ongeldig.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL verhul scan staat is ongeldig.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL wanaanpassing scan staat is ongeldig.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Skandering URLs staat is ongeldig.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing handtekeninge scan staat is ongeldig.';
+$lang['antivirus_twice_a_day'] = 'Twee keer &#39;n dag';
+$lang['antivirus_update_interval'] = 'Werk Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Aantal updates per dag is ongeldig.';
