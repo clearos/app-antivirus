e1c476a700c6475e1e13372edacae5c101db5ab1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:19:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Nu gateway Antipirus app ngagunakeun mesin antipirus puseur pikeun nyeken web, ftp, mail jeung leuwih. Eta ngajaga alat disambungkeun ka jaringan Anjeun ku stopping malware saméméh ngabogaan kasempetan pikeun ngahontal pamaké anjeun.';
+$lang['antivirus_app_name'] = 'gateway Antipirus';
+$lang['antivirus_app_tooltip'] = 'payil énkripsi anu mindeng dipaké pikeun nganteurkeun malware, jadi mangga kudu ati ngeunaan ngamungkinkeun tipe ieu koropak ngaliwatan mesin antipirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok nilai arsip énkripsi nyaéta sah.';
+$lang['antivirus_block_encrypted_files'] = 'Blok énkripsi Payil';
+$lang['antivirus_every_two_hours'] = 'Unggal Dua Jam';
+$lang['antivirus_maximum_file_size'] = 'Maksimum Ukuran File di pos Payil';
+$lang['antivirus_maximum_file_size_invalid'] = 'ukuran file nu maksimum téh sah.';
+$lang['antivirus_maximum_files'] = 'Payil maksimum dina pos Payil';
+$lang['antivirus_maximum_files_invalid'] = 'jumlah maksimum payil anu sah.';
+$lang['antivirus_maximum_recursion'] = 'Maksimum Recursion di pos Payil';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursion nu maksimum téh sah.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL state scan cloaking téh sah.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch state scan téh sah.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'state Scan URL anu sah.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing tanda tangan nyeken state téh sah.';
+$lang['antivirus_twice_a_day'] = 'Dua kali Poé a';
+$lang['antivirus_update_interval'] = 'Update selang';
+$lang['antivirus_updates_per_day_invalid'] = 'Jumlah apdet per poé téh sah.';
