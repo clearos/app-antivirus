7c9a8035f84222b8f92bcb2da287dff89bd13f46
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:18:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pt/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'O aplicativo gateway antivírus usa um motor antivírus central para digitalizar web, FTP, e-mail e muito mais. Ele protege os dispositivos conectados à sua rede, parando malware antes que ele tenha a chance de alcançar seus usuários.';
+$lang['antivirus_app_name'] = 'gateway de antivírus';
+$lang['antivirus_app_tooltip'] = 'Os arquivos criptografados são muitas vezes utilizados para entregar malware, por isso tenha cuidado sobre permitir que esses tipos de arquivos através do mecanismo antivírus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Bloco valor de arquivo criptografado é inválido.';
+$lang['antivirus_block_encrypted_files'] = 'Bloquear arquivos criptografados';
+$lang['antivirus_every_two_hours'] = 'A cada duas horas';
+$lang['antivirus_maximum_file_size'] = 'Tamanho máximo do arquivo em arquivos Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'tamanho máximo do arquivo é inválido.';
+$lang['antivirus_maximum_files'] = 'Arquivos máximos em arquivos Zip';
+$lang['antivirus_maximum_files_invalid'] = 'Número máximo de arquivos é inválido.';
+$lang['antivirus_maximum_recursion'] = 'A recursão máxima em arquivos Zip';
+$lang['antivirus_maximum_recursion_invalid'] = 'recursão máxima é inválido.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL estado varredura cloaking é inválido.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL incompatibilidade estado de verificação é inválido.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Estado URLs Scan é inválido.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing assinaturas digitalizar estado é inválido.';
+$lang['antivirus_twice_a_day'] = 'Duas vezes ao dia';
+$lang['antivirus_update_interval'] = 'Intervalo de atualização';
+$lang['antivirus_updates_per_day_invalid'] = 'Número de atualizações por dia é inválido.';
