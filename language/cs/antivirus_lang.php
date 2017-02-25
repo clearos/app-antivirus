47a4dee48248560af8e4eac674c0308c3a8435ed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:12:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus Aplikace využívá centrální antivirový program ke skenování web, FTP, pošty a další. Chrání zařízení připojená k síti zastavením malware před tím, než má šanci oslovit uživatele.';
+$lang['antivirus_app_name'] = 'brána Antivirus';
+$lang['antivirus_app_tooltip'] = 'Šifrované soubory jsou často využívána k doručování malware, takže buďte opatrní ohledně povolení tyto typy souborů pomocí antivirového programu.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok šifrované archiv hodnota je neplatná.';
+$lang['antivirus_block_encrypted_files'] = 'Blok šifrovaných souborů';
+$lang['antivirus_every_two_hours'] = 'Každé dvě hodiny';
+$lang['antivirus_maximum_file_size'] = 'Maximální velikost souboru ve formátu ZIP souborů';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maximální velikost souboru je neplatná.';
+$lang['antivirus_maximum_files'] = 'Maximální počet souborů ve formátu ZIP soubory';
+$lang['antivirus_maximum_files_invalid'] = 'Maximální počet souborů je neplatná.';
+$lang['antivirus_maximum_recursion'] = 'Maximální rekurze na zip soubory';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximální rekurze je neplatný.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL maskování stav skenování je neplatný.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL nesoulad URL skenování stav je neplatný.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URL stav je neplatný.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing podpisy kontrolovat stav je neplatný.';
+$lang['antivirus_twice_a_day'] = 'Dvakrát denně';
+$lang['antivirus_update_interval'] = 'Interval aktualizace';
+$lang['antivirus_updates_per_day_invalid'] = 'Počet aktualizací denně je neplatný.';
