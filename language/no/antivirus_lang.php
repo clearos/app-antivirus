7e868d6ef5d30296cf5783580200caa1b828aa7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:17:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app bruker en sentral antivirusmotor til å skanne web, FTP, e-post og mer. Det beskytter enheter som er koblet til nettverket ved å stoppe malware før den har en sjanse til å nå brukerne.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Krypterte filer blir ofte brukt til å levere malware, så vær forsiktig med å tillate slike filer via antivirusmotor.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block kryptert arkiv verdien er ugyldig.';
+$lang['antivirus_block_encrypted_files'] = 'Block krypterte filer';
+$lang['antivirus_every_two_hours'] = 'Annenhver time';
+$lang['antivirus_maximum_file_size'] = 'Maksimal filstørrelse i zip-filer';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimal filstørrelse er ugyldig.';
+$lang['antivirus_maximum_files'] = 'Maksimalt antall filer i zip-filer';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimalt antall filer er ugyldig.';
+$lang['antivirus_maximum_recursion'] = 'Maksimal Rekursjon i zip-filer';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimal rekursjon er ugyldig.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL maskering skanning staten er ugyldig.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch skanning staten er ugyldig.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan webadresser staten er ugyldig.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing signaturer skanne staten er ugyldig.';
+$lang['antivirus_twice_a_day'] = 'To ganger om dagen';
+$lang['antivirus_update_interval'] = 'oppdaterings~~POS=TRUNC';
+$lang['antivirus_updates_per_day_invalid'] = 'Antall oppdateringer per dag er ugyldig.';
