a03d35d48b53e3ea7fecc2dfbbcef6ea30a3ada3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:13:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Η εφαρμογή πύλη Antivirus χρησιμοποιεί ένα κεντρικό μηχανισμό προστασίας από ιούς για να σαρώσετε web, FTP, ταχυδρομείο και πολλά άλλα. Προστατεύει τις συσκευές που συνδέονται στο δίκτυό σας με την παύση κακόβουλο λογισμικό πριν να έχει την ευκαιρία να φτάσει τους χρήστες σας.';
+$lang['antivirus_app_name'] = 'πύλη Antivirus';
+$lang['antivirus_app_tooltip'] = 'Τα κρυπτογραφημένα αρχεία που χρησιμοποιούνται συχνά για να παραδώσει malware, γι &#39;αυτό παρακαλώ να είστε προσεκτικοί σχετικά επιτρέποντας αυτούς τους τύπους αρχείων μέσω της μηχανής antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Αποκλεισμός κρυπτογραφημένη τιμή αρχείο δεν είναι έγκυρο.';
+$lang['antivirus_block_encrypted_files'] = 'Αποκλεισμός κρυπτογραφημένα αρχεία';
+$lang['antivirus_every_two_hours'] = 'Κάθε δύο ώρες';
+$lang['antivirus_maximum_file_size'] = 'Μέγιστο μέγεθος αρχείου σε zip αρχεία';
+$lang['antivirus_maximum_file_size_invalid'] = 'Μέγιστο μέγεθος αρχείου δεν είναι έγκυρο.';
+$lang['antivirus_maximum_files'] = 'Μέγιστη Αρχεία σε αρχεία Zip';
+$lang['antivirus_maximum_files_invalid'] = 'Μέγιστος αριθμός αρχείων είναι έγκυρη.';
+$lang['antivirus_maximum_recursion'] = 'Μέγιστη Αναδρομή στην Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Η μέγιστη αναδρομή είναι έγκυρη.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL απόκρυψη κατάσταση σάρωσης δεν είναι έγκυρο.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL αναντιστοιχία URL κατάσταση σάρωσης δεν είναι έγκυρο.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Σάρωση των διευθύνσεων URL κράτος είναι άκυρο.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing υπογραφές σάρωση κατάσταση δεν είναι έγκυρο.';
+$lang['antivirus_twice_a_day'] = 'Δύο φορές την ημέρα';
+$lang['antivirus_update_interval'] = 'Ενημέρωση Διάστημα';
+$lang['antivirus_updates_per_day_invalid'] = 'Αριθμό των ενημερώσεων ανά ημέρα δεν είναι έγκυρο.';
