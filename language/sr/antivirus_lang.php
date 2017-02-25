e490f9f647099299b43ca9b275fb9d207be6c59d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:18:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Гатеваи антивирус апликација користи централно антивирусни мотор за скенирање веб, ФТП, поште и још много тога. Она штити уређаје који су повезани са мрежом заустављањем малваре пре него што има прилику да постигну своје кориснике.';
+$lang['antivirus_app_name'] = 'гатеваи антивирус';
+$lang['antivirus_app_tooltip'] = 'Шифроване датотеке се често користе за испоруку малваре, па будите опрезни око дозвољавања ове врсте фајлова преко антивирус мотор.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Блок кодиран Архива вредност је неважећа.';
+$lang['antivirus_block_encrypted_files'] = 'Блок шифроване датотеке';
+$lang['antivirus_every_two_hours'] = 'Свака два сата';
+$lang['antivirus_maximum_file_size'] = 'Максимална величина датотеке у Зип Филес';
+$lang['antivirus_maximum_file_size_invalid'] = 'Максимална величина датотеке је неважећи.';
+$lang['antivirus_maximum_files'] = 'Максималан број фајлова у Зип Филес';
+$lang['antivirus_maximum_files_invalid'] = 'Максималан број фајлова је неважећи.';
+$lang['antivirus_maximum_recursion'] = 'Максимални Рекурзија у Зип Филес';
+$lang['antivirus_maximum_recursion_invalid'] = 'Максимални рецурсион је неважећи.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'ССЛ скривање скенирање држава је неважећи.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'ССЛ УРЛ адреса неслагање скенирање држава је неважећи.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Скенирање УРЛ држава је неважећи.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Пхисхинг потписа скенирање стање је неважећи.';
+$lang['antivirus_twice_a_day'] = 'Два пута на дан';
+$lang['antivirus_update_interval'] = 'Интервал ажурирања';
+$lang['antivirus_updates_per_day_invalid'] = 'Број ажурирања дневно је неважећи.';
