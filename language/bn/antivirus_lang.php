bda4fc833be094aa8558761f51d15f9879f40bb0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:11:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'গেটওয়ে অ্যান্টিভাইরাস অ্যাপ্লিকেশন ওয়েব, এফটিপি, মেইল ​​এবং আরো স্ক্যান করার জন্য একটি কেন্দ্রীয় অ্যান্টিভাইরাস ইঞ্জিন ব্যবহার করে. এটা ম্যালওয়্যার-সেভার বন্ধ করার এটি আপনার ব্যবহারকারীদের কাছে পৌঁছানোর জন্য একটি সুযোগ রয়েছে দ্বারা আপনার নেটওয়ার্কের সাথে সংযুক্ত ডিভাইস থেকে রক্ষা করে.';
+$lang['antivirus_app_name'] = 'গেটওয়ে অ্যান্টিভাইরাস';
+$lang['antivirus_app_tooltip'] = 'এনক্রিপ্ট করা ফাইল প্রায়ই ম্যালওয়ার রক্ষা করার জন্য, তাই দয়া করে অ্যান্টিভাইরাস ইঞ্জিন দিয়ে ফাইল এই ধরনের অনুমতি সম্পর্কে সতর্কতা অবলম্বন করা আবশ্যক ব্যবহার করা হয়.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'ব্লক এনক্রিপ্ট সংরক্ষণাগার মান অবৈধ.';
+$lang['antivirus_block_encrypted_files'] = 'ব্লক এনক্রিপ্ট করা ফাইল';
+$lang['antivirus_every_two_hours'] = 'প্রতি দুই ঘন্টা';
+$lang['antivirus_maximum_file_size'] = 'জিপ ফাইল সর্বাধিক ফাইলের আকার';
+$lang['antivirus_maximum_file_size_invalid'] = 'সর্বাধিক ফাইলের আকার অবৈধ.';
+$lang['antivirus_maximum_files'] = 'জিপ ফাইল সর্বাধিক ফাইল';
+$lang['antivirus_maximum_files_invalid'] = 'ফাইলের সর্বোচ্চ নম্বর অবৈধ.';
+$lang['antivirus_maximum_recursion'] = 'জিপ ফাইল সর্বাধিক Recursion';
+$lang['antivirus_maximum_recursion_invalid'] = 'সর্বোচ্চ, recursion অবৈধ.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'এসএসএল cloaking স্ক্যানের রাষ্ট্র অবৈধ.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'এসএসএল URL টি মেলেনি স্ক্যানের রাষ্ট্র অবৈধ.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'স্ক্যান URL গুলি রাষ্ট্র অবৈধ.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'ফিশিং স্বাক্ষর রাষ্ট্র স্ক্যান অবৈধ.';
+$lang['antivirus_twice_a_day'] = 'দিনে দুবার';
+$lang['antivirus_update_interval'] = 'আপডেট ব্যবধান';
+$lang['antivirus_updates_per_day_invalid'] = 'প্রতিদিন আপডেট সংখ্যা অবৈধ.';
