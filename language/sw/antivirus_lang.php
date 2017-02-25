3141d08078a872f3ad786fcadc2af0b1433881cd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:19:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'programu Gateway Antivirus anatumia kati antivirus injini Scan mtandao, FTP, barua pepe na zaidi. Ni kulinda vifaa kushikamana na mtandao wako na kuacha zisizo kabla ina nafasi ya kufikia watumiaji yako.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'files encrypted mara nyingi hutumika kwa kutoa zisizo, hivyo tafadhali kuwa makini kuhusu kuruhusu aina hii ya mafaili kwa njia ya injini antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block encrypted archive thamani ni batili.';
+$lang['antivirus_block_encrypted_files'] = 'Block fiche Files';
+$lang['antivirus_every_two_hours'] = 'Kila Hours Mbili';
+$lang['antivirus_maximum_file_size'] = 'Upeo File Size katika Zip Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Upeo faili ni batili.';
+$lang['antivirus_maximum_files'] = 'Upeo Files katika Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Upeo wa idadi ya files ni batili.';
+$lang['antivirus_maximum_recursion'] = 'Upeo Recursion katika Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Upeo kujirudia ni batili.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan hali ni batili.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch scan hali ni batili.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'hali Scan URLs ni batili.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Hadaa saini Scan hali ni batili.';
+$lang['antivirus_twice_a_day'] = 'Mara mbili Day';
+$lang['antivirus_update_interval'] = 'Mwisho Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Idadi ya updates kwa siku ni batili.';
