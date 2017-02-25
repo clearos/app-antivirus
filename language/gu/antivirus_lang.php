5a79112cd6e93b3bfc654c6d2774b3eec6ba93d7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:13:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'ગેટવે એન્ટિવાયરસ એપ્લિકેશન વેબ, FTP, મેલ અને વધુ સ્કેન કરવા માટે કેન્દ્રીય એન્ટીવાયરસ એન્જિન વાપરે છે. તે મૉલવેર અટકાવવા તે પહેલાં તમારા વપરાશકર્તાઓ સુધી પહોંચવા માટે એક તક છે દ્વારા તમારા નેટવર્ક સાથે જોડાયેલ ઉપકરણો રક્ષણ આપે છે.';
+$lang['antivirus_app_name'] = 'ગેટવે એન્ટિવાયરસ';
+$lang['antivirus_app_tooltip'] = 'એનક્રિપ્ટ થયેલ ફાઈલો વારંવાર મૉલવેર વિતરિત, તેથી કૃપા કરીને એન્ટીવાયરસ એન્જિન મારફતે ફાઈલો આ પ્રકારના માટે પરવાનગી આપે છે વિશે સાવચેત રહો ઉપયોગ થાય છે.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'બ્લોક એનક્રિપ્ટ થયેલ આર્કાઇવ કિંમત અમાન્ય છે.';
+$lang['antivirus_block_encrypted_files'] = 'બ્લોક એનક્રિપ્ટ થયેલ ફાઈલો';
+$lang['antivirus_every_two_hours'] = 'દર બે કલાક';
+$lang['antivirus_maximum_file_size'] = 'ઝિપ ફાઈલો મહત્તમ ફાઈલ માપ';
+$lang['antivirus_maximum_file_size_invalid'] = 'મહત્તમ ફાઈલ માપ અમાન્ય છે.';
+$lang['antivirus_maximum_files'] = 'ઝિપ ફાઈલો માં વધુમાં વધુ ફાઇલો';
+$lang['antivirus_maximum_files_invalid'] = 'ફાઇલો ની મહત્તમ સંખ્યા અમાન્ય છે.';
+$lang['antivirus_maximum_recursion'] = 'ઝિપ ફાઈલો મહત્તમ રિકર્ઝન';
+$lang['antivirus_maximum_recursion_invalid'] = 'મહત્તમ રિકર્ઝન અમાન્ય છે.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking સ્કેન રાજ્ય અમાન્ય છે.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL ખોટી જોડણી સ્કેન રાજ્ય અમાન્ય છે.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'સ્કેન URL ને રાજ્ય અમાન્ય છે.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'ફિશીંગ સહીઓ રાજ્ય સ્કેન અમાન્ય છે.';
+$lang['antivirus_twice_a_day'] = 'બે દિવસ';
+$lang['antivirus_update_interval'] = 'સુધારા અંતરાલ';
+$lang['antivirus_updates_per_day_invalid'] = 'દિવસ દીઠ સુધારાઓ સંખ્યા અમાન્ય છે.';
