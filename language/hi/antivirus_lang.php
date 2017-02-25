f14026ce4c6e23baff88664ea0c4596bff815a86
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:13:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'गेटवे एंटीवायरस एप्लिकेशन वेब, एफ़टीपी, मेल और अधिक स्कैन करने के लिए एक केंद्रीय एंटीवायरस इंजन का उपयोग करता है। यह मैलवेयर रोक से पहले यह अपने उपयोगकर्ताओं तक पहुँचने के लिए एक मौका है द्वारा अपने नेटवर्क से जुड़े उपकरणों की रक्षा करता है।';
+$lang['antivirus_app_name'] = 'गेटवे एंटीवायरस';
+$lang['antivirus_app_tooltip'] = 'एन्क्रिप्टेड फ़ाइलें अक्सर मैलवेयर वितरित करने के लिए है, तो कृपया एंटीवायरस इंजन के माध्यम से फाइल के इन प्रकार की अनुमति के बारे में सावधान रहना इस्तेमाल कर रहे हैं।';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'ब्लॉक एन्क्रिप्टेड संग्रह मान अमान्य है।';
+$lang['antivirus_block_encrypted_files'] = 'ब्लॉक एनक्रिप्टेड फ़ाइलें';
+$lang['antivirus_every_two_hours'] = 'हर दो घंटे';
+$lang['antivirus_maximum_file_size'] = 'ज़िप फ़ाइलों में अधिकतम फ़ाइल आकार';
+$lang['antivirus_maximum_file_size_invalid'] = 'अधिकतम फ़ाइल आकार अमान्य है।';
+$lang['antivirus_maximum_files'] = 'ज़िप फ़ाइलों में अधिकतम फ़ाइलें';
+$lang['antivirus_maximum_files_invalid'] = 'फ़ाइलों की अधिकतम संख्या अमान्य है।';
+$lang['antivirus_maximum_recursion'] = 'ज़िप फ़ाइलों में अधिकतम Recursion';
+$lang['antivirus_maximum_recursion_invalid'] = 'अधिकतम प्रत्यावर्तन अमान्य है।';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'एसएसएल क्लोकिंग स्कैन राज्य अमान्य है।';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'एसएसएल यूआरएल बेमेल स्कैन राज्य अमान्य है।';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'स्कैन यूआरएल राज्य अमान्य है।';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'फ़िशिंग हस्ताक्षर राज्य स्कैन अमान्य है।';
+$lang['antivirus_twice_a_day'] = 'दिन में दो बार';
+$lang['antivirus_update_interval'] = 'अंतराल का आधुनिकीकरण';
+$lang['antivirus_updates_per_day_invalid'] = 'प्रति दिन अद्यतन की संख्या अमान्य है।';
