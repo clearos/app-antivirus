9a35b2a3a9d693867ad9f9022fe2d05fbde04d3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:12:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'Gateway Antivirus app koristi centralni antivirusni motor za skeniranje web, FTP, e-mail i još mnogo toga. Štiti uređaje povezane na mrežu zaustavljanje zlonamjernih programa prije nego što ima priliku doći do svoje korisnike.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Šifrirane datoteke često koriste za isporuku malware, pa budite oprezni o dopuštajući ove vrste datoteka putem antivirusni motor.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blok kodiran arhiva vrijednost nije valjana.';
+$lang['antivirus_block_encrypted_files'] = 'Blok šifrirane datoteke';
+$lang['antivirus_every_two_hours'] = 'Svaka dva sata';
+$lang['antivirus_maximum_file_size'] = 'Maksimalna veličina datoteke u zip datotekama';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimalna veličina datoteke nije valjan.';
+$lang['antivirus_maximum_files'] = 'Maksimalno Datoteke u Zip datoteke';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimalni broj datoteka nije valjana.';
+$lang['antivirus_maximum_recursion'] = 'Maksimalna rekurzije u Zip datoteke';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimalna rekurzija je nevažeći.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking stanje skeniranje nije valjan.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL neslaganje stanje skeniranje nije valjan.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Skeniranje URL-ova država nije valjan.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing potpisi skenirati stanje nije valjan.';
+$lang['antivirus_twice_a_day'] = 'Dva puta dnevno';
+$lang['antivirus_update_interval'] = 'Interval ažuriranja';
+$lang['antivirus_updates_per_day_invalid'] = 'Broj promjenama po danu nije valjan.';
