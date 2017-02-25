9ee3399b5e616501eb03d33fab36ec3c3ae4a589
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:13:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app yana amfani da wani riga-kafi tsakiya engine to duba yanar gizo, FTP, mail da kuma more. Yana kare na&#39;urorin da alaka to your cibiyar sadarwa da tsayawa malware da shi yana da damar isa ga masu amfani.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Rufaffen files sukan amfani da su isar malware, don haka don Allah yi hankali game da kyale wadannan iri fayiloli, ta hanyar riga-kafi engine.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block zane archive darajar ba daidai ba ne.';
+$lang['antivirus_block_encrypted_files'] = 'Block rufaffen Files';
+$lang['antivirus_every_two_hours'] = 'Kowane Biyu Hours';
+$lang['antivirus_maximum_file_size'] = 'Maximum File Size a akwatin gidan Files';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maximum file size ba daidai ba ne.';
+$lang['antivirus_maximum_files'] = 'Maximum Files a akwatin gidan Files';
+$lang['antivirus_maximum_files_invalid'] = 'Matsakaicin yawan files ba daidai ba ne.';
+$lang['antivirus_maximum_recursion'] = 'Maximum Recursion a akwatin gidan Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximum recursion ba daidai ba ne.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking scan jihar ba daidai ba ne.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch scan jihar ba daidai ba ne.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Scan URLs jihar ba daidai ba ne.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Mai leƙan asirri sa hannu duba jihar ba daidai ba ne.';
+$lang['antivirus_twice_a_day'] = 'Sau biyu a rana';
+$lang['antivirus_update_interval'] = 'Update tazara';
+$lang['antivirus_updates_per_day_invalid'] = 'Number of updates a kowace rana shi ne daidai ba.';
