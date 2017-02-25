2abc49ff23f9f41318215c260debfcc161c8d524
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:14:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'A Gateway Antivirus alkalmazás használja a központi vírusvédelmi motor olvasni web, FTP, e-mail és így tovább. Védi eszközök csatlakozik a hálózathoz megállás malware, mielőtt azt a lehetőséget, hogy elérje a felhasználók számára.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'A titkosított fájlok gyakran szállít malware, kérjük, legyen óvatos, amely lehetővé teszi az ilyen típusú fájlokat a víruskereső motort.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blokk titkosított archív érték érvénytelen.';
+$lang['antivirus_block_encrypted_files'] = 'Blokk Titkosított fájlok';
+$lang['antivirus_every_two_hours'] = 'Kétóránként';
+$lang['antivirus_maximum_file_size'] = 'Maximális fájlméret Zip fájlok';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maximális fájlméret érvénytelen.';
+$lang['antivirus_maximum_files'] = 'Maximális fájlok Zip fájlok';
+$lang['antivirus_maximum_files_invalid'] = 'A fájlok maximális száma érvénytelen.';
+$lang['antivirus_maximum_recursion'] = 'Maximális rekurzió Zip fájlok';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maximális rekurzió érvénytelen.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL álcázó szkennelés állapota nem megfelelő.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch szkennelés állapota nem megfelelő.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'URL-ek vizsgálata állapota nem megfelelő.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Az adathalászat aláírások beolvasni állapota nem megfelelő.';
+$lang['antivirus_twice_a_day'] = 'Naponta kétszer';
+$lang['antivirus_update_interval'] = 'frissítési időköz';
+$lang['antivirus_updates_per_day_invalid'] = 'Frissítések számát naponta érvénytelen.';
