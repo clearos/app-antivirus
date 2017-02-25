a32a3201af98490c59a35a1892d428a3178acaa9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:14:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus aplikasi menggunakan mesin antivirus pusat untuk memindai web, FTP, mail dan banyak lagi. Ini melindungi perangkat yang terhubung ke jaringan Anda dengan menghentikan malware sebelum memiliki kesempatan untuk menjangkau pengguna Anda.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'file terenkripsi yang sering digunakan untuk menyampaikan malware, jadi harap berhati-hati tentang memungkinkan jenis file melalui mesin antivirus.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Blokir nilai arsip dienkripsi tidak valid.';
+$lang['antivirus_block_encrypted_files'] = 'Blok Encrypted File';
+$lang['antivirus_every_two_hours'] = 'Setiap Dua Jam';
+$lang['antivirus_maximum_file_size'] = 'Maksimum Ukuran File di File Zip';
+$lang['antivirus_maximum_file_size_invalid'] = 'Ukuran file maksimum tidak valid.';
+$lang['antivirus_maximum_files'] = 'File maksimum Zip Files';
+$lang['antivirus_maximum_files_invalid'] = 'Jumlah maksimum file tidak valid.';
+$lang['antivirus_maximum_recursion'] = 'Maksimum Rekursi di Zip Files';
+$lang['antivirus_maximum_recursion_invalid'] = 'rekursi maksimum tidak valid.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL negara pemindaian cloaking tidak valid.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL mismatch negara scan tidak valid.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'negara Scan URL tidak valid.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing tanda tangan memindai negara tidak valid.';
+$lang['antivirus_twice_a_day'] = 'Dua kali sehari';
+$lang['antivirus_update_interval'] = 'Update Interval';
+$lang['antivirus_updates_per_day_invalid'] = 'Jumlah update per hari tidak valid.';
