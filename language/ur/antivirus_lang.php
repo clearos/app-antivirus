719d081f385061cab02fbba8b1b4a3d2ad14a956
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:21:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'گیٹ وے Antivirus اپلی کیشن ویب، FTP، میل اور زیادہ اسکین کرنے کے ایک مرکزی ینٹیوائرس انجن کا استعمال کرتا. یہ میلویئر روکنے یہ آپ کے صارفین تک پہنچنے کے لئے ایک موقع ہے اس سے پہلے کی طرف سے آپ کے نیٹ ورک سے منسلک آلات کی حفاظت کرتا ہے.';
+$lang['antivirus_app_name'] = 'گیٹ وے اینٹی وائرس';
+$lang['antivirus_app_tooltip'] = 'خفیہ کردہ فائلوں اکثر میلویئر فراہم کرنے کے لئے، تاکہ ینٹیوائرس کے انجن کے ذریعے فائلوں کی ان اقسام کی اجازت دی کے بارے میں ہوشیار رہنا براہ مہربانی استعمال کیا جاتا ہے.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'بلاک مرموز محفوظ شدہ دستاویزات قدر غلط ہے.';
+$lang['antivirus_block_encrypted_files'] = 'بلاک خفیہ کردہ فائلوں';
+$lang['antivirus_every_two_hours'] = 'ہر دو گھنٹے';
+$lang['antivirus_maximum_file_size'] = 'زپ فائل میں زیادہ سے زیادہ فائل سائز';
+$lang['antivirus_maximum_file_size_invalid'] = 'زیادہ سے زیادہ فائل سائز غلط ہے.';
+$lang['antivirus_maximum_files'] = 'زپ فائل میں زیادہ سے زیادہ فائلوں';
+$lang['antivirus_maximum_files_invalid'] = 'فائلوں کی زیادہ سے زیادہ نمبر غلط ہے.';
+$lang['antivirus_maximum_recursion'] = 'زپ فائل میں زیادہ سے زیادہ تکرار';
+$lang['antivirus_maximum_recursion_invalid'] = 'زیادہ سے زیادہ تکرار غلط ہے.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL cloaking کے اسکین ریاست باطل ہے.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL بیمیل اسکین ریاست باطل ہے.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'سکین کے URLs ریاست باطل ہے.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'دستخط ریاست اسکین فشنگ غلط ہے.';
+$lang['antivirus_twice_a_day'] = 'دن میں دو بار';
+$lang['antivirus_update_interval'] = 'اپ ڈیٹ کا دورانیہ';
+$lang['antivirus_updates_per_day_invalid'] = 'فی دن اپ ڈیٹ کی تعداد غلط ہے.';
