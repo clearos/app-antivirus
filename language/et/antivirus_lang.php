0e2b064c3168311ab176d1580bac39bac4f4ca70
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:12:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'The Gateway Antivirus app kasutab keskne viirusetõrje mootor skaneerida web, FTP, posti ja rohkem. See kaitseb ühendatud võrku, peatades pahavara enne kui see on võimalus, et jõuda oma kasutajatele.';
+$lang['antivirus_app_name'] = 'Gateway Antivirus';
+$lang['antivirus_app_tooltip'] = 'Krüpteeritud failide kasutatakse sageli pakkuda pahavara, nii et palun olla ettevaatlik võimaldab seda tüüpi failid läbi viirusetõrje mootor.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'Block krüpteeritud arhiiv väärtus on vigane.';
+$lang['antivirus_block_encrypted_files'] = 'Block krüptitud faile';
+$lang['antivirus_every_two_hours'] = 'Iga kahe tunni';
+$lang['antivirus_maximum_file_size'] = 'Maksimaalne faili suurus zip faile';
+$lang['antivirus_maximum_file_size_invalid'] = 'Maksimaalne faili suurus ei sobi.';
+$lang['antivirus_maximum_files'] = 'Maksimaalne failide ZIP-failid';
+$lang['antivirus_maximum_files_invalid'] = 'Maksimaalne failide arv on kehtetu.';
+$lang['antivirus_maximum_recursion'] = 'Maksimaalne Recursion zip faile';
+$lang['antivirus_maximum_recursion_invalid'] = 'Maksimaalne rekursioon on kehtetu.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'SSL peitmise skaneerimise riik on kehtetu.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'SSL URL ebakõla skaneerimise riik on kehtetu.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'Skaneerimine URL riik on kehtetu.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'Phishing allkirjad skaneerida riik on kehtetu.';
+$lang['antivirus_twice_a_day'] = 'Kaks korda päevas';
+$lang['antivirus_update_interval'] = 'värskendamise intervalli';
+$lang['antivirus_updates_per_day_invalid'] = 'Number uudiseid päevas ei sobi.';
