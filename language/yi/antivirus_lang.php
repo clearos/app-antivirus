6a9bb37c5174e6f1784d583ad92303d3a897335d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 13:21:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/antivirus_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+$lang['antivirus_app_description'] = 'די גייטוויי אַנטיווירוס אַפּ ניצט אַ צענטראל אַנטיווירוס מאָטאָר צו יבערקוקן וועב, FTP, פּאָסט און מער. עס פּראַטעקץ דיווייסאַז קאָננעקטעד צו דיין נעץ דורך סטאָפּפּינג מאַלוואַרע איידער עס האט אַ געלעגנהייַט צו דערגרייכן דיין ניצערס.';
+$lang['antivirus_app_name'] = 'גייטוויי אַנטיווירוס';
+$lang['antivirus_app_tooltip'] = 'ינקריפּטיד טעקעס זענען אָפֿט געניצט צו באַפרייַען מאַלוואַרע, אַזוי ביטע זיין אָפּגעהיט וועגן אַלאַוינג די טייפּס פון טעקעס דורך די אַנטיווירוס מאָטאָר.';
+$lang['antivirus_block_encrypted_archive_flag_invalid'] = 'בלאַק ענקריפּטיד אַרקייוו ווערט איז פאַרקריפּלט.';
+$lang['antivirus_block_encrypted_files'] = 'בלאַק ינקריפּטיד טעקעס';
+$lang['antivirus_every_two_hours'] = 'יעדער צוויי שעה';
+$lang['antivirus_maximum_file_size'] = 'מאַקסימום טעקע גרייס אין זיפּ טעקעס';
+$lang['antivirus_maximum_file_size_invalid'] = 'מאַקסימום טעקע גרייס איז פאַרקריפּלט.';
+$lang['antivirus_maximum_files'] = 'מאַקסימום טעקעס אין זיפּ טעקעס';
+$lang['antivirus_maximum_files_invalid'] = 'מאַקסימום נומער פון טעקעס איז פאַרקריפּלט.';
+$lang['antivirus_maximum_recursion'] = 'מאַקסימום רעקורסיאָן אין זיפּ טעקעס';
+$lang['antivirus_maximum_recursion_invalid'] = 'מאַקסימום רעקורסיאָן איז פאַרקריפּלט.';
+$lang['antivirus_phishing_always_block_cloak_invalid'] = 'ססל קלאָאַקינג יבערקוקן שטאַט איז פאַרקריפּלט.';
+$lang['antivirus_phishing_always_block_ssl_mismatch_invalid'] = 'ססל URL מיסמאַטש יבערקוקן שטאַט איז פאַרקריפּלט.';
+$lang['antivirus_phishing_scan_urls_state_invalid'] = 'סקאַן URL ס שטאַט איז פאַרקריפּלט.';
+$lang['antivirus_phishing_signatures_state_invalid'] = 'פישינג סיגנאַטשערז יבערקוקן שטאַט איז פאַרקריפּלט.';
+$lang['antivirus_twice_a_day'] = 'צוויי מאָל אַ טאָג';
+$lang['antivirus_update_interval'] = 'אַפּדייט ינטערוואַל';
+$lang['antivirus_updates_per_day_invalid'] = 'נומער פון אַפּדייץ פּער טאָג איז פאַרקריפּלט.';
